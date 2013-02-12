6aec921d011a8e15bf400c35c937122f7f27f465
Jens Halm <halm@spicefactory.org>
2013-02-12 03:32:45 +0100

    extract common parsers into new base trait

---
+++ src/main/scala/laika/parse/rst/BlockBaseParsers.scala
@@ -0,0 +1,108 @@
+package laika.parse.rst
+
+import laika.tree.Elements._
+import scala.annotation.tailrec
+
+trait BlockBaseParsers extends laika.parse.BlockParsers {
+
+  
+  val tabStops = 8
+  
+  
+  /** The maximum level of block nesting. Some block types like lists
+   *  and blockquotes contain nested blocks. To protect against malicious
+   *  input or accidentally broken markup, the level of nesting is restricted.
+   */
+  val maxNestLevel: Int = 12
+  
+  
+  case class BlockPosition (nestLevel: Int, column: Int) {
+    
+    //def nextNestLevel = new BlockPosition(nestLevel + 1, column)
+    
+    def indent (value: Int) = new BlockPosition(nestLevel + 1, column + value)
+    
+  }
+  
+  
+  /** Parses all of the standard reStructuredText blocks, except normal paragraphs. 
+   * 
+   *  @param pos the current parsing position 
+   */
+  def standardRstBlock (pos: BlockPosition): Parser[Block]
+
+  /** Parses reStructuredText blocks, except normal paragraphs
+   *  and blocks that allow nesting of blocks. Only used in rare cases when the maximum
+   *  nest level allowed had been reached
+   */
+  def nonRecursiveRstBlock: Parser[Block]
+  
+  def paragraph: Parser[Paragraph]
+  
+  
+  override def ws = anyOf(' ','\t', '\f', '\u000b') // 0x0b: vertical tab
+  
+  
+  def indent (current: Int, expect: Int = 0) = Parser { in =>
+    
+    val source = in.source
+    val eof = source.length
+
+    val finalIndent = if (expect > 0) current + expect else Int.MaxValue
+    
+    def result (offset: Int, indent: Int) = Success(indent - current, in.drop(offset - in.offset))
+    
+    @tailrec
+    def parse (offset: Int, indent: Int): ParseResult[Int] = {
+      if (offset == eof) Failure("unecpected end of input", in)
+      else if (indent == finalIndent) result(offset, indent)
+      else {
+        source.charAt(offset) match {
+          case ' ' | '\f' | '\u000b' => parse(offset + 1, indent + 1)
+          case '\t' => val newIndent = (indent / tabStops + 1) * tabStops
+                       if (newIndent > finalIndent) result(offset, indent) else parse(offset + 1, newIndent)
+          case _    => if (indent < finalIndent && expect > 0) Failure("Less than expected indentation", in) else result(offset, indent)
+        }
+        
+      }
+    }
+  
+    parse(in.offset, current)
+  }
+  
+  
+  def minIndent (current: Int, min: Int) = indent(current) ^? { case i if i >= min => i }
+    
+  
+  /** Parses a full block based on the specified helper parser. When the parser for
+   *  the first line succeeds, this implementation assumes that for any subsequent
+   *  lines or blocks the only requirement is that they are not empty and indented
+   *  like the first line.
+   * 
+   *  @param firstLinePrefix parser that recognizes the start of the first line of this block
+   *  @param pos the current parsing position 
+   */
+  def indentedBlock (firstLinePrefix: Parser[Int], pos: BlockPosition): Parser[(List[String],BlockPosition)] = {
+    indentedBlock(firstLinePrefix, not(blankLine), not(blankLine), pos)
+  }
+  
+  /** Parses a full block based on the specified helper parsers. It expects an indentation for
+   *  all subsequent lines based on the length of the prefix of the first line plus any whitespace
+   *  immediately following it.
+   * 
+   *  @param firstLinePrefix parser that recognizes the start of the first line of this block
+   *  @param linePrefix parser that recognizes the start of subsequent lines that still belong to the same block
+   *  @param nextBlockPrefix parser that recognizes whether a line after one or more blank lines still belongs to the same block
+   *  @param pos the current parsing position 
+   */
+  def indentedBlock (firstLinePrefix: Parser[Int], linePrefix: => Parser[Any], nextBlockPrefix: => Parser[Any], pos: BlockPosition): Parser[(List[String],BlockPosition)] = {
+    firstLinePrefix >> { width => minIndent(pos.column + width, 1) >> { firstIndent =>
+      val indentParser = indent(pos.column, width + firstIndent)
+      block(success( () ), indentParser ~> linePrefix, indentParser ~> nextBlockPrefix) ^^ { lines => 
+        (lines, pos.indent(firstIndent))   
+      } 
+    }}  
+  }
+  
+  
+}
--- src/main/scala/laika/parse/rst/ListParsers.scala
+++ src/main/scala/laika/parse/rst/ListParsers.scala
@@ -23,53 +23,8 @@ import scala.annotation.tailrec
 import scala.collection.mutable.Stack
 import scala.collection.mutable.ListBuffer
 
-trait ListParsers extends laika.parse.BlockParsers { self: InlineParsers => // TODO - probably needs to be rst.InlineParsers {
+trait ListParsers extends BlockBaseParsers { self: InlineParsers => // TODO - probably needs to be rst.InlineParsers {
 
-  
-  // TODO - several members need to be extracted to a base trait once more features get added
-  
-  
-  val tabStops = 8
-  
-  
-  /** The maximum level of block nesting. Some block types like lists
-   *  and blockquotes contain nested blocks. To protect against malicious
-   *  input or accidentally broken markup, the level of nesting is restricted.
-   */
-  val maxNestLevel: Int = 12
-  
-  
-  case class BlockPosition (nestLevel: Int, column: Int) {
-    
-    //def nextNestLevel = new BlockPosition(nestLevel + 1, column)
-    
-    def indent (value: Int) = new BlockPosition(nestLevel + 1, column + value)
-    
-  }
-  
-  
-  /** Parses blocks that may appear inside a list item.
-   * 
-   *  @param pos the current parsing position 
-   */
-  def listItemBlocks (pos: BlockPosition) = 
-    if (pos.nestLevel < maxNestLevel) (standardRstBlock(pos) | paragraph) *
-    else (nonRecursiveRstBlock | paragraph) *
-   
-  
-  /** Parses all of the standard reStructuredText blocks, except normal paragraphs. 
-   * 
-   *  @param pos the current parsing position 
-   */
-  def standardRstBlock (pos: BlockPosition): Parser[Block] = unorderedList(pos) | orderedList(pos) | definitionList(pos) | fieldList(pos) | optionList(pos)
-
-  /** Parses reStructuredText blocks, except normal paragraphs
-   *  and blocks that allow nesting of blocks. Only used in rare cases when the maximum
-   *  nest level allowed had been reached
-   */
-  def nonRecursiveRstBlock: Parser[Block]
-  
-  def paragraph: Parser[Paragraph]
   
   /*
    * unordered list:
@@ -125,71 +80,15 @@ trait ListParsers extends laika.parse.BlockParsers { self: InlineParsers => // T
    * - any blank line ends the block
    */
   
-  override def ws = anyOf(' ','\t', '\f', '\u000b') // 0x0b: vertical tab
   
-  
-  
-  def indent (current: Int, expect: Int = 0) = Parser { in =>
-    
-    val source = in.source
-    val eof = source.length
-
-    val finalIndent = if (expect > 0) current + expect else Int.MaxValue
-    
-    def result (offset: Int, indent: Int) = Success(indent - current, in.drop(offset - in.offset))
-    
-    @tailrec
-    def parse (offset: Int, indent: Int): ParseResult[Int] = {
-      if (offset == eof) Failure("unecpected end of input", in)
-      else if (indent == finalIndent) result(offset, indent)
-      else {
-        source.charAt(offset) match {
-          case ' ' | '\f' | '\u000b' => parse(offset + 1, indent + 1)
-          case '\t' => val newIndent = (indent / tabStops + 1) * tabStops
-                       if (newIndent > finalIndent) result(offset, indent) else parse(offset + 1, newIndent)
-          case _    => if (indent < finalIndent && expect > 0) Failure("Less than expected indentation", in) else result(offset, indent)
-        }
-        
-      }
-    }
-  
-    parse(in.offset, current)
-  }
-  
-  
-  def minIndent (current: Int, min: Int) = indent(current) ^? { case i if i >= min => i }
-    
-  
-  /** Parses a full block based on the specified helper parser. When the parser for
-   *  the first line succeeds, this implementation assumes that for any subsequent
-   *  lines or blocks the only requirement is that they are not empty and indented
-   *  like the first line.
+  /** Parses blocks that may appear inside a list item.
    * 
-   *  @param firstLinePrefix parser that recognizes the start of the first line of this block
    *  @param pos the current parsing position 
    */
-  def indentedBlock (firstLinePrefix: Parser[Int], pos: BlockPosition): Parser[(List[String],BlockPosition)] = {
-    indentedBlock(firstLinePrefix, not(blankLine), not(blankLine), pos)
-  }
-  
-  /** Parses a full block based on the specified helper parsers. It expects an indentation for
-   *  all subsequent lines based on the length of the prefix of the first line plus any whitespace
-   *  immediately following it.
-   * 
-   *  @param firstLinePrefix parser that recognizes the start of the first line of this block
-   *  @param linePrefix parser that recognizes the start of subsequent lines that still belong to the same block
-   *  @param nextBlockPrefix parser that recognizes whether a line after one or more blank lines still belongs to the same block
-   *  @param pos the current parsing position 
-   */
-  def indentedBlock (firstLinePrefix: Parser[Int], linePrefix: => Parser[Any], nextBlockPrefix: => Parser[Any], pos: BlockPosition): Parser[(List[String],BlockPosition)] = {
-    firstLinePrefix >> { width => minIndent(pos.column + width, 1) >> { firstIndent =>
-      val indentParser = indent(pos.column, width + firstIndent)
-      block(success( () ), indentParser ~> linePrefix, indentParser ~> nextBlockPrefix) ^^ { lines => 
-        (lines, pos.indent(firstIndent))   
-      } 
-    }}  
-  }
-  
+  def listItemBlocks (pos: BlockPosition) = 
+    if (pos.nestLevel < maxNestLevel) (standardRstBlock(pos) | paragraph) *
+    else (nonRecursiveRstBlock | paragraph) *
+    
   
   /** Parses a single list item.
    * 
