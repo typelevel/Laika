5bfdf9615bf34252fb6913927eb82c5063624455
Jens Halm <halm@spicefactory.org>
2013-03-29 16:48:45 +0100

    fix the parsing of names inside field lists

--- src/main/scala/laika/parse/rst/ListParsers.scala
+++ src/main/scala/laika/parse/rst/ListParsers.scala
@@ -172,7 +172,7 @@ trait ListParsers extends BlockBaseParsers { self: InlineParsers =>
    */
   def fieldList: Parser[Block] = {
     
-    val name = ':' ~> anyBut(':') <~ ':' ~ (guard(eol) | ' ') // TODO - escaped ':' in name should be supported
+    val name = ':' ~> (anyBut(':') min 1)  <~ ':' ~ (guard(eol) | ' ') // TODO - escaped ':' in name should be supported
     
     val item = (name ~ opt(varIndentedBlock())) ^^
       { case name ~ Some(block) => 
