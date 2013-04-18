dd0a789aff30170908d5bf31fba0ed4837e36908
Jens Halm <halm@spicefactory.org>
2013-04-18 03:05:04 +0200

    complete scaladoc for ReStructuredText class

--- src/main/scala/laika/parse/rst/ReStructuredText.scala
+++ src/main/scala/laika/parse/rst/ReStructuredText.scala
@@ -30,11 +30,32 @@ import laika.parse.rst.TextRoles._
  *  Transform from ReStructuredText to HTML fromFile "hello.rst" toFile "hello.html"
  *  }}}
  * 
- *  reStructuredText has several types of extension points that Laika supports.
+ *  reStructuredText has several types of extension points that are fully supported by Laika.
+ *  In contrast to the original Python implementation, the API has been redesigned to be a more
+ *  idiomatic, concise and type-safe Scala DSL.
  * 
- *  TODO - examples and pointers to rst spec
- *  
- *  
+ *  The following extension types are available:
+ * 
+ *  $ - Block Directives - an extension hook for adding new block level elements to
+ *      reStructuredText markup. Use the `withBlockDirectives` method of this class to
+ *      add directive implementations to the parser. Specification entry: 
+ *      [[http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html#directives]]
+ * 
+ *  $ - Substitution Definitions - an extension hook for adding new span level elements to
+ *      reStructuredText markup that can be used by substitution references (like `|subst|`). 
+ *      Use the `withSpanDirectives` method of this class to
+ *      add directive implementations to the parser that can be used as substitution definitions. 
+ *      Specification entry: 
+ *      [[http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html#substitution-definitions]]
+ * 
+ *  $ - Interpreted Text Roles - an extension hook for adding new dynamic span level elements to
+ *      reStructuredText markup. In contrast to substitution definitions the implementation of a text
+ *      role uses the text from the occurrences in the markup referring to the role as input.
+ *      Use the `withTextRoles` method of this class to
+ *      add custom text role implementations to the parser that can be referred to by interpreted text. 
+ *      Specification entry: 
+ *      [[http://docutils.sourceforge.net/docs/ref/rst/directives.html#custom-interpreted-text-roles]]
+ * 
  *  @author Jens Halm
  */
 class ReStructuredText private (
@@ -44,22 +65,66 @@ class ReStructuredText private (
     ) extends (Input => Document) { self =>
 
   
-  /** Adds the specified directives to the parser.
+  /** Adds the specified directives and returns a new instance of the parser.
    *  These block directives may then be used anywhere in documents parsed by this instance.
+   * 
+   *  Example:
+   * 
+   *  {{{
+   *  case class Note (title: String, content: Seq[Block]) extends Block with BlockContainer[Note]
+   *
+   *  val rst = ReStructuredText withBlockDirectives (
+   *    BlockDirective("note") {
+   *      (argument() ~ blockContent)(Note)
+   *    }
+   *  )                                              
+   *
+   *  Transform from rst to HTML fromFile "hello.rst" toFile "hello.html"
+   *  }}}   
+   * 
+   *  For more details on implementing directives see [[laika.parse.rst.Directives]].
    */
   def withBlockDirectives (directives: Directive[Block]*) = {
     new ReStructuredText(blockDirectives ++ directives, spanDirectives, textRoles)    
   }
      
-  /** Adds the specified directives to the parser.
+  /** Adds the specified directives and returns a new instance of the parser.
    *  These span directives can then be referred to by substitution references.
+   * 
+   *  Example:
+   * 
+   *  {{{
+   *  val rst = ReStructuredText withSpanDirectives (
+   *    SpanDirective("replace") {
+   *      spanContent map SpanSequence
+   *    }
+   *  ) 
+   * 
+   *  Transform from rst to HTML fromFile "hello.rst" toFile "hello.html"   
+   *  }}}
+   * 
+   *  For more details on implementing directives see [[laika.parse.rst.Directives]].
    */ 
   def withSpanDirectives (directives: Directive[Span]*) = {
     new ReStructuredText(blockDirectives, spanDirectives ++ directives, textRoles)    
   }
   
-  /** Adds the specified text roles to the parser.
+  /** Adds the specified text roles and returns a new instance of the parser.
    *  These text roles may then be used in interpreted text spans.
+   *  
+   *  Example:
+   * 
+   *  {{{
+   *  val rst = ReStructuredText withTextRoles (
+   *    TextRole("link", "http://www.our-server.com/tickets/")(field("base-url")) {
+   *      (base, text) => Link(List(Text(text)), base + text)
+   *    }
+   *  )
+   *   
+   *  Transform from rst to HTML fromFile "hello.rst" toFile "hello.html"   
+   *  }}}
+   * 
+   *  For more details on implementing directives see [[laika.parse.rst.TextRoles]].
    */
   def withTextRoles (roles: TextRole*) = {
     new ReStructuredText(blockDirectives, spanDirectives, textRoles ++ roles)    
