47f2b32662c2ace7d62976036c9ec7c5896d59d3
Jens Halm <halm@spicefactory.org>
2013-07-10 22:22:26 +0200

    align use of ModelBuilder for creating link reference elements

--- src/test/scala/laika/parse/rst/InlineParsersSpec.scala
+++ src/test/scala/laika/parse/rst/InlineParsersSpec.scala
@@ -40,15 +40,15 @@ class InlineParsersSpec extends FlatSpec
   def subst (name: String) = SubstitutionReference(name)
   
   
-  def pLinkRef (id: String, text: String): LinkReference = LinkReference(List(Text(text)), id, "`"+text+"`_")
+  def pLinkRef (id: String, text: String): LinkReference = linkRef(txt(text)).id(id).source("`"+text+"`_")
 
   def pLinkRef (id: String): LinkReference = pLinkRef(id,id)
   
-  def anonPLinkRef (text: String) = LinkReference(List(Text(text)), "", "`" + text + "`__")
+  def anonPLinkRef (text: String) = linkRef(txt(text)).source("`" + text + "`__")
   
-  def linkRef (id: String): LinkReference = LinkReference(List(Text(id)), id, id+"_")
+  def linkRef (id: String): LinkReference = linkRef(txt(id)).id(id).source(id+"_")
   
-  def anonLinkRef (text: String) = LinkReference(List(Text(text)), "", text+"__")
+  def anonLinkRef (text: String) = linkRef(txt(text)).source(text+"__")
   
   
   
