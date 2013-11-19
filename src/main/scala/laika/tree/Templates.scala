a87f3a56c55262165000f1b12da8bd93f2e14436
Jens Halm <halm@spicefactory.org>
2013-11-19 03:29:40 +0100

    add scaladoc for new template tree elements

--- src/main/scala/laika/tree/Templates.scala
+++ src/main/scala/laika/tree/Templates.scala
@@ -22,16 +22,54 @@ import laika.tree.Elements._
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
+/** Provides the elements of the document tree which are specific to templates.
+ *  
+ *  In contrast to document trees built from text markup, these elements
+ *  do not make a distinction between block and inline elements. Therefore
+ *  all template elements mix in the `TemplateSpan` trait which extends
+ *  `Span`, making it an inline element.
+ *  
+ *  The abstract base classes are not sealed as the tree model is extensible.
+ *  Renderers should anticipate unknown elements and add fallback rules for those.
+ *  
+ *  @author Jens Halm
+ */
 object Templates {
   
+  /** Represents a placeholder inline element that needs
+   *  to be resolved in a rewrite step.
+   *  Useful for elements that need access to the
+   *  document, structure, title or configuration before
+   *  being fully resolved.
+   */
   trait SpanResolver extends Span {
     def resolve (context: DocumentContext): Span
   }
 
+  /** Represents a placeholder block element that needs
+   *  to be resolved in a rewrite step.
+   *  Useful for elements that need access to the
+   *  document, structure, title or configuration before
+   *  being fully resolved.
+   */
   trait BlockResolver extends Block {
     def resolve (context: DocumentContext): Block
   }
   
+  /** Represents a reference to a value from the context
+   *  of the current document. The `ref` attribute
+   *  is a simple path expression in dot notation
+   *  for accessing properties of an object (or keys
+   *  of a Map).
+   *  
+   *  The root elements accessible to such a reference are:
+   *  
+   *  - `document`: the current document with all of its public properties
+   *  - `parent`: the parent tree of the current document
+   *  - `root`: the root tree
+   *  - `config`: all configuration values for the current document, 
+   *    including those inherited from parent trees
+   */
   abstract class ContextReference[T <: Span] (ref: String) extends SpanResolver {
     def result (value: Option[Any]): T
     def resolve (context: DocumentContext): Span = context.resolveReference(ref) match {
@@ -40,6 +78,8 @@ object Templates {
     }
   }
   
+  /** A context reference specifically for use in template documents.
+   */
   case class TemplateContextReference (ref: String, options: Options = NoOpt) extends ContextReference[TemplateSpan](ref) with TemplateSpan {
     def result (value: Option[Any]): TemplateSpan = value match {
       case Some(s: TemplateSpan) => s
@@ -49,6 +89,8 @@ object Templates {
     }
   }
   
+  /** A context reference specifically for use in markup documents.
+   */
   case class MarkupContextReference (ref: String, options: Options = NoOpt) extends ContextReference[Span](ref) {
     def result (value: Option[Any]): Span = value match {
       case Some(s: Span)    => s
@@ -59,20 +101,45 @@ object Templates {
   }
 
   
+  /** The base type for all inline elements that
+   *  can be found in a template.
+   */
   trait TemplateSpan extends Span
-  
+ 
+  /** Wraps a generic element that otherwise could not be placed directly into
+   *  a template document tree. Useful when custom tags which are placed inside
+   *  a template produce non-template tree elements.
+   */
   case class TemplateElement (element: Element, options: Options = NoOpt) extends TemplateSpan with ElementTraversal[TemplateElement]
 
+  /** A generic container element containing a list of template spans. Can be used where a sequence
+   *  of spans must be inserted in a place where a single element is required by the API.
+   *  Usually renderers do not treat the container as a special element and render its children
+   *  as s sub flow of the parent container.
+   */
   case class TemplateSpanSequence (content: Seq[TemplateSpan], options: Options = NoOpt) extends TemplateSpan with SpanContainer[TemplateSpanSequence]
 
+  /** A simple string element, representing the parts of a template
+   *  that are not detected as special markup constructs and treated as raw text.
+   */
   case class TemplateString (content: String, options: Options = NoOpt) extends TemplateSpan with TextContainer
   
+  /** The root element of a template document tree.
+   */
   case class TemplateRoot (content: Seq[TemplateSpan], options: Options = NoOpt) extends Block with SpanContainer[TemplateRoot]
   
+  /** A template document containing the element tree of a parsed template and its extracted
+   *  configuration section (if present).
+   */
   case class TemplateDocument (path: Path, content: TemplateRoot, config: Config = ConfigFactory.empty) {
     
     val name = path.name
     
+    /** Rewrites this template document, replacing all
+     *  span and block resolvers with the final resolved
+     *  element they produce based on the specified
+     *  document context.
+     */
     def rewrite (context: DocumentContext) = {
       val newContent = content rewrite rewriteRules(context)
       context.document.withRewrittenContent(RootElement(Seq(newContent)), context.document.fragments, context.autonumbering.number)
@@ -80,6 +147,12 @@ object Templates {
     
   }
   
+  /** The default rewrite rules for template documents,
+   *  responsible for replacing all
+   *  span and block resolvers with the final resolved
+   *  element they produce based on the specified
+   *  document context.
+   */
   def rewriteRules (context: DocumentContext) = {
     lazy val rule: PartialFunction[Element, Option[Element]] = {
       case ph: BlockResolver => Some(rewriteChild(ph resolve context))
