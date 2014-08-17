15874b4673152ec05da704f43e5f3aa5fccf6d5e
Jens Halm <halm@spicefactory.org>
2014-08-17 14:45:16 +0200

    add tests for CSSParsers

---
+++ core/src/test/scala/laika/parse/css/CSSParsersSpec.scala
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.parse.css
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import laika.parse.helper.DefaultParserHelpers
+import laika.parse.helper.ParseResultHelpers
+import laika.tree.Templates._
+import laika.parse.css.Styles._
+
+class CSSParsersSpec extends FlatSpec 
+                        with Matchers 
+                        with CSSParsers
+                        with ParseResultHelpers 
+                        with DefaultParserHelpers[Set[StyleDeclaration]] {
+
+  
+  val defaultParser: Parser[Set[StyleDeclaration]] = styleDeclarationSet
+  
+  
+  val defaultStyleMap = Map("foo"->"bar")
+  
+  def selector (predicates: Predicate*) = Selector(predicates.toSet)
+  
+  def selector (selector: Selector, parent: Selector, immediate: Boolean) =
+    selector.copy(parent = Some(ParentSelector(parent, immediate)))
+  
+  def styleDecl (selector: Selector) = 
+    StyleDeclaration(selector, defaultStyleMap)
+
+  def styleDecl (predicates: Predicate*) = StyleDeclaration(Selector(predicates.toSet), defaultStyleMap)
+
+  def styleDecl (styles: Map[String,String], predicates: Predicate*) = StyleDeclaration(Selector(predicates.toSet), styles)
+  
+  
+  
+  "The CSS parser" should "parse a style with a type selector" in {
+    
+    val css = """Type {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(ElementType("Type"))))
+  }
+  
+  it should "parse a style with a class selector" in {
+    
+    val css = """.class {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(StyleName("class"))))
+  }
+  
+  it should "parse a style with an id selector" in {
+    
+    val css = """#id {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(Id("id"))))
+  }
+  
+  it should "parse a style with a child selector" in {
+    
+    val css = """Type .class {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(selector(selector(StyleName("class")), selector(ElementType("Type")), false))))
+  }
+  
+  it should "parse a style with an immediate child selector" in {
+    
+    val css = """Type > .class {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(selector(selector(StyleName("class")), selector(ElementType("Type")), true))))
+  }
+  
+  it should "parse a style with multiple child selectors" in {
+    
+    val css = """Type .class > #id {
+      | foo: bar;
+      |}""".stripMargin
+    
+    val top = selector(ElementType("Type"))
+    val middle = selector(selector(StyleName("class")), top, false)
+    val finalSelector = selector(selector(Id("id")), middle, true)
+    Parsing (css) should produce (Set(styleDecl(finalSelector)))
+  }
+  
+  it should "parse a style with multiple selectors" in {
+    
+    val css = """Type, .class {
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(StyleName("class")).increaseOrderBy(1), styleDecl(ElementType("Type"))))
+  }
+  
+  it should "parse multiple style declarations" in {
+    
+    val css = """Type {
+      |  foo: bar;
+      |}
+      |
+      |.class {
+      |  bar: foo;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(Map("bar"->"foo"), StyleName("class")).increaseOrderBy(1), styleDecl(ElementType("Type"))))
+  }
+  
+  it should "parse multiple style declarations with comments" in {
+    
+    val css = """/* comment 1 */
+      |Type {
+      |  foo: bar;
+      |}
+      |
+      |/* comment 2 */
+      |
+      |.class {
+      |  bar: foo;
+      |}
+      |
+      |/* comment 3 */""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(Map("bar"->"foo"), StyleName("class")).increaseOrderBy(1), styleDecl(ElementType("Type"))))
+  }
+  
+  it should "parse a style with multiple comments" in {
+    
+    val css = """Type {
+      | /* comment 1 */
+      | /* comment 2 */ foo: bar;
+      | /* comment 3 */
+      | bar: foo; /* comment 4 */
+      | /* comment 5 */
+      | foo: bar;
+      |}""".stripMargin
+    
+    Parsing (css) should produce (Set(styleDecl(Map("foo"->"bar", "bar"->"foo"), ElementType("Type"))))
+  }
+  
+  
+}
