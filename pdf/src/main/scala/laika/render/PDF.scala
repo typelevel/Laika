7ed63bb45e973038c725d92140d48a8bf1c8b1b9
Jens Halm <halm@spicefactory.org>
2016-01-09 14:23:50 +0000

    PDF renderer: add missing scaladoc sections and return types for public members

--- pdf/src/main/scala/laika/render/PDF.scala
+++ pdf/src/main/scala/laika/render/PDF.scala
@@ -16,21 +16,22 @@
 
 package laika.render
 
-import java.io.OutputStream
-import java.io.StringReader
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.stream.StreamSource
-import javax.xml.transform.sax.SAXResult
 import laika.factory.RenderResultProcessor
-import laika.tree.Documents.DocumentTree
 import laika.io.Output
 import laika.io.Output.BinaryOutput
 import laika.io.OutputProvider.OutputConfig
+import laika.tree.Documents.DocumentTree
+import laika.tree.Elements.Element
+import laika.tree.Elements.MessageLevel
+import java.io.File
+import java.io.OutputStream
+import java.io.StringReader
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.URIResolver
+import javax.xml.transform.sax.SAXResult
+import javax.xml.transform.stream.StreamSource
 import org.apache.fop.apps.FopFactory
 import org.apache.xmlgraphics.util.MimeConstants
-import javax.xml.transform.URIResolver
-import java.io.File
-import laika.tree.Elements.MessageLevel
 
 /** A post processor for PDF output, based on an interim XSL-FO renderer. 
  *  May be directly passed to the `Render` or `Transform` APIs:
@@ -41,6 +42,9 @@ import laika.tree.Elements.MessageLevel
  *  
  *  Transform from Markdown to PDF fromDirectory "book-src" toFile "book.pdf"
  *  }}}
+ *  
+ *  In the second example above the input from an entire directory gets
+ *  merged into a single output directory.
  * 
  *  @author Jens Halm
  */
@@ -53,18 +57,43 @@ class PDF private (val factory: XSLFO, config: PDFConfig) extends RenderResultPr
   /** Specifies the minimum required level for a system message
    *  to get included into the output by this renderer.
    */
-  def withMessageLevel (level: MessageLevel) = new PDF(factory.withMessageLevel(level), config)
+  def withMessageLevel (level: MessageLevel): PDF = new PDF(factory.withMessageLevel(level), config)
   
-  def withConfig (config: PDFConfig) = new PDF(factory, config)
+  /** Allows to specify configuration options like insertion
+   *  of bookmarks or table of content.
+   */
+  def withConfig (config: PDFConfig): PDF = new PDF(factory, config)
   
   private lazy val foForPDF = new FOforPDF(config)
   
-  
+
+  /** Renders the XSL-FO that serves as a basis for producing the final PDF output.
+   *  The result should include the output from rendering the documents in the 
+   *  specified tree as well as any additional insertions like bookmarks or
+   *  table of content. For this the specified `DocumentTree` instance may get
+   *  modified before passing it to the given render function.
+   *  
+   *  The default implementation simply delegates to an instance of `FOforPDF`
+   *  which uses a `PDFConfig` object to drive configuration. In rare cases
+   *  where the flexibility provided by `PDFConfig` is not sufficient, this
+   *  method may get overridden.
+   * 
+   *  @param tree the document tree serving as input for the renderer
+   *  @param render the actual render function for producing the XSL-FO output
+   *  @return the rendered XSL-FO as a String 
+   */
   protected def renderFO (tree: DocumentTree, render: (DocumentTree, OutputConfig) => Unit): String =
     foForPDF.renderFO(tree, render)
   
-    
-  def process (tree: DocumentTree, render: (DocumentTree, OutputConfig) => Unit, output: BinaryOutput) = {
+  /** Processes the tree by first using the specified render function
+   *  to produce the interim XSL-FO result, transform the result to PDF and write
+   *  it to the specified final output.
+   * 
+   *  @param tree the tree to render to the interim result
+   *  @param render the render function for producing the interim XSL-FO result
+   *  @param output the output to write the final result to
+   */  
+  def process (tree: DocumentTree, render: (DocumentTree, OutputConfig) => Unit, output: BinaryOutput): Unit = {
     
     def createSAXResult (out: OutputStream) = {
       val foUserAgent = fopFactory.newFOUserAgent
@@ -104,9 +133,23 @@ class PDF private (val factory: XSLFO, config: PDFConfig) extends RenderResultPr
  */
 object PDF extends PDF(XSLFO.unformatted, PDFConfig.default)
 
+/** Configuration options for the generated PDF output.
+ * 
+ *  @param treeTitles indicates whether a title will be inserted for each tree and subtree, 
+ *  relying on titles specified in the tree configuration file
+ *  @param docTitles indicates whether a title will be inserted for each document, 
+ *  relying on `Title` elements in the document or titles specified in the document configuration header
+ *  @param bookmarks indicates whether PDF bookmarks should be inserted into the PDF document
+ *  @param toc indicates whether a table of content should be inserted at the beginning of the PDF document
+ */
 case class PDFConfig(treeTitles: Boolean = true, docTitles: Boolean = true, bookmarks: Boolean = true, toc: Boolean = true)
 
+/** Companion for creation `PDFConfig` instances.
+ */
 object PDFConfig {
-  val default = apply()
+  
+  /** The default configuration, with all optional features enabled.
+   */
+  val default: PDFConfig = apply()
 }
 
