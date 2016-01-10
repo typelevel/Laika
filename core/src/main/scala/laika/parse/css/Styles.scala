51127c7860be07f362d874e375765ff38d2f9e9b
Jens Halm <halm@spicefactory.org>
2016-01-10 23:41:37 +0000

    Styles: add scaladoc and return types

--- core/src/main/scala/laika/parse/css/Styles.scala
+++ core/src/main/scala/laika/parse/css/Styles.scala
@@ -23,43 +23,96 @@ import scala.language.existentials
 import laika.tree.Documents.Path
 import laika.tree.Documents.Root
 
-/**
- * @author Jens Halm
+/** Provides the domain model for the CSS engine.
+ *  
+ *  The available selector types represent a subset of the full
+ *  CSS standard, picking those features which are reasonably suited
+ *  in the context of the Laika document model. It includes type,
+ *  class and id selectors, but no attribute selectors as most
+ *  Laika tree elements primarily contain a sequence of children
+ *  and not many other properties to match against.
+ *  
+ *  It also supports the CSS syntax to declare selectors for
+ *  child elements (e.g. `Paragraph .intro`) or immediate
+ *  children (e.g. `Paragraph > .intro`).
+ * 
+ *  @author Jens Halm
  */
 object Styles {
 
   
+  /** Represents a single predicate which is
+   *  part of the selector for a style declaration.
+   */
   sealed trait Predicate {
+    
+    /** The specificity of this predicate.
+     *  Used to calculate the precedence if
+     *  multiple selectors apply to the same
+     *  target element.
+     */
     def specificity: Specificity
+    
+    /** Indicates whether this predicate holds
+     *  for the specified element.
+     */
     def evaluate (element: Element): Boolean
   }
-  
+
+  /** A predicate that holds if the given type name matches the type 
+   *  of the target element. In contrast to web CSS, in Laika 
+   *  this is not a tag name, but the (unqualified) name
+   *  of the class of the target element instance (e.g. `Paragraph`).  
+   */
   case class ElementType (name: String) extends Predicate {
     val specificity = Specificity(0,0,1,0)
-    def evaluate (element: Element) = element.getClass.getSimpleName == name
+    def evaluate (element: Element): Boolean = element.getClass.getSimpleName == name
   }
-  
+
+  /** A predicate that holds if the given id matches
+   *  the id of the target element. 
+   */
   case class Id (id: String) extends Predicate {
     val specificity = Specificity(1,0,0,0)
-    def evaluate (element: Element) = element match {
+    def evaluate (element: Element): Boolean = element match {
       case c: Customizable if c.options.id == Some(id) => true
       case _ => false
     }
   }  
   
+  /** A predicate that holds if the given style name matches
+   *  one of the styles of the target element. 
+   */
   case class StyleName (name: String) extends Predicate {
     val specificity = Specificity(0,1,0,0)
-    def evaluate (element: Element) = element match {
+    def evaluate (element: Element): Boolean = element match {
       case c: Customizable if c.options.styles.contains(name) => true
       case _ => false
     }
   }  
   
+  /** Represents a parent selector and indicates whether it is
+   *  an immediate parent or an ancestor anywhere in the hierarchy.
+   */
   case class ParentSelector (selector: Selector, immediate: Boolean)
   
+  /** Represents the specificity of a selector or predicate.
+   *  This is modeled after the precedence rules of the CSS standard.  
+   */
   case class Specificity (ids: Int, classes: Int, types: Int, order: Int) extends Ordered[Specificity] {
-    def + (other: Specificity) = Specificity(ids + other.ids, classes + other.classes, types + other.types, order)
-    def compare (other: Specificity) = 
+    
+    /** Adds this and the given specificity by summing
+     *  all its individual parameters.
+     */
+    def + (other: Specificity): Specificity = 
+      Specificity(ids + other.ids, classes + other.classes, types + other.types, order)
+
+    /** Compares this instance with the given specificity.
+     *  Applies the CSS standard weighting of first comparing the
+     *  number of specified ids, followed by classes, then types 
+     *  and finally the order. 
+     */
+    def compare (other: Specificity): Int = 
       if (ids != other.ids) ids compare other.ids
       else if (classes != other.classes) classes compare other.classes
       else if (types != other.types) types compare other.types
@@ -67,16 +120,40 @@ object Styles {
   }
   
   
+  /** Responsible for determining whether a style declaration
+   *  should be applied to a target element, basing its decision
+   *  on a set of predicates.
+   *  
+   *  @param predicates the set of predicates that need to hold
+   *  for this selector to be applicable to a target element
+   *  @param parent the optional parent selector
+   *  @param order the index of the style declaration this selector
+   *  belongs to
+   */
   case class Selector (
       predicates: Set[Predicate] = Set(), 
       parent: Option[ParentSelector] = None,
       order: Int = 0) {
     
+    /** The specificity of this selector, calculated
+     *  from the specificity of its predicates and the order.
+     *  
+     *  Used to calculate the precedence if multiple selectors 
+     *  apply to the same target element.
+     */
     def specificity: Specificity = {
       val thisSpec = predicates.map(_.specificity).reduceLeftOption(_+_).getOrElse(Specificity(0,0,0,0)).copy(order = order)
       parent.fold(thisSpec)(thisSpec + _.selector.specificity)
     }
     
+    /** Indicates whether this selector applies to the specified target
+     *  element.
+     * 
+     *  @param target the target element to apply this selector to
+     *  @param parents the parents of the specified target element, which
+     *  also need to match in case this selector has parent selectors.
+     *  @return true if this selector matches the target element
+     */
     def matches (target: Element, parents: Seq[Element]): Boolean = {
       def matchesParent (parents: Seq[Element], selector: Selector): Boolean = parents match {
         case Seq() => false
@@ -93,38 +170,80 @@ object Styles {
   }
   
   
+  /** Represents a single style declaration.
+   * 
+   *  @param selector the selector to determine which elemens this declaration applies to
+   *  @param styles the styles themselves in a map representing the names and values of each style
+   */
   case class StyleDeclaration (selector: Selector, styles: Map[String, String]) {
     
-    def appliesTo (element: Element, parents: Seq[Element]) = selector.matches(element, parents)
+    /** Indicates whether this style declaration applies to the specified target
+     *  element.
+     * 
+     *  @param target the target element to apply the selector of this style declaration to
+     *  @param parents the parents of the specified target element, which
+     *  also need to match in case the selector of this declaration has parent selectors.
+     *  @return true if this style declaration applies to the target element
+     */
+    def appliesTo (element: Element, parents: Seq[Element]): Boolean = selector.matches(element, parents)
     
-    def increaseOrderBy (amount: Int) = 
+    /** Returns a new style declaration with the order parameter
+     *  in its Specificity property increased by the specified amount.
+     */
+    def increaseOrderBy (amount: Int): StyleDeclaration = 
       if (amount == 0) this
       else StyleDeclaration(selector.copy(order = selector.order + amount), styles)
     
   }
-  
+
+  /** Companion providing factory methods for the StyleDeclaration class.
+   */
   object StyleDeclaration extends ((Selector, Map[String, String]) => StyleDeclaration) {
+    
+    /** Creates a new StyleDeclaration with only one predicate.
+     */
     def apply(predicate: Predicate, styles: (String, String)*): StyleDeclaration = apply(Selector(Set(predicate)), styles.toMap)
   }
   
+  /** Represents a set of one or more style declarations. 
+   * 
+   *  @param paths the paths the style declarations have been obtained from
+   *  @param styles the style declarations that belong to this set
+   */
   case class StyleDeclarationSet (paths: Set[Path], styles: Set[StyleDeclaration]) {
     
-    def collectStyles (element: Element, parents: Seq[Element]) = {
-      val decls = styles.filter(_.appliesTo(element, parents)).toSeq.sortBy(_.selector.specificity)
+    /** Collects all the styles that apply to the specified target element. 
+     * 
+     *  @param target the target element to collect the matching styles for
+     *  @param parents the parents of the specified target element
+     *  @return a map representing the keys and values of all styles that apply to the target element
+     */
+    def collectStyles (target: Element, parents: Seq[Element]): Map[String, String] = {
+      val decls = styles.filter(_.appliesTo(target, parents)).toSeq.sortBy(_.selector.specificity)
       (Map[String,String]() /: decls) { case (acc, decl) => acc ++ decl.styles }
     }
     
-    def ++ (set: StyleDeclarationSet) = {
+    /** Merges the style declaration of this instance with the specified set
+     *  and returns the merged set in a new instance.
+     */
+    def ++ (set: StyleDeclarationSet): StyleDeclarationSet = {
       val maxOrder = if (styles.isEmpty) 0 else styles.maxBy(_.selector.order).selector.order + 1
       new StyleDeclarationSet(paths ++ set.paths, styles ++ (set.styles map (_.increaseOrderBy(maxOrder))))
     }
     
   }
   
+  /** Companion providing factory methods for the StyleDeclaration class.
+   */
   object StyleDeclarationSet extends ((Set[Path], Set[StyleDeclaration]) => StyleDeclarationSet) {
     
-    val empty = new StyleDeclarationSet(Set(Root), Set.empty) 
-    
+    /** Creates an empty StyleDeclarationSet with `Root` as its only path element.
+     */
+    val empty: StyleDeclarationSet = new StyleDeclarationSet(Set(Root), Set.empty) 
+
+    /** Creates a new StyleDeclarationSet with a single path element and
+     *  the specified declarations.
+     */
     def apply(path: Path, styles: StyleDeclaration*): StyleDeclarationSet = apply(Set(path), styles.toSet)
     
   }
