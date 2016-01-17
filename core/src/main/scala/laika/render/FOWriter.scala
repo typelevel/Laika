1a8df8fb534990cea93b8354aca879ae28ff7a4d
Jens Halm <halm@spicefactory.org>
2016-01-17 15:46:29 +0000

    add scaladoc to FOWriter

--- core/src/main/scala/laika/render/FOWriter.scala
+++ core/src/main/scala/laika/render/FOWriter.scala
@@ -42,11 +42,11 @@ class FOWriter (out: String => Unit,
                                            with FOProperties {
 
   
-  protected def attributes (tag: String, options: Options, attrs: Seq[(String,Any)]) = {
+  protected def attributes (tag: String, options: Options, attrs: Seq[(String,Any)]): Seq[(String, Any)] = {
     filterAttributes(tag, ("id"->options.id) +: attrs)
   }
   
-  protected def attributes (tag: String, element: Element, attrs: Seq[(String,Any)]) = {
+  protected def attributes (tag: String, element: Element, attrs: Seq[(String,Any)]): Seq[(String, Any)] = {
     val fromCSS = styles.collectStyles(element, parents).toSeq
     val options = element match {
       case c: Customizable => c.options
@@ -56,53 +56,83 @@ class FOWriter (out: String => Unit,
   }
   
   
+  /** Generates a unique id for the specified path of the target
+   *  document and the local reference.
+   */
   def buildId (path: Path, ref: String): String = {
     val treePath = if (path.parent == Root) "" else path.parent.toString.replaceAllLiterally("/", "_")
     val docPath = if (path == Root) "" else treePath + "_" + path.basename
     docPath + "_" + ref
   }
   
-  def blockContainer (element: Element, content: Seq[Block], attr: (String,String)*) = 
+  /** Renders an FO `block` element, containing nested blocks.
+   */
+  def blockContainer (element: Element, content: Seq[Block], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:block", element, attr: _*) <<|> content <<| "</fo:block>"
   
-  def listBlock (element: Element, content: Seq[ListItem], attr: (String,String)*) = 
+  /** Renders an FO `list-block` element, and the specified list items.
+   */
+  def listBlock (element: Element, content: Seq[ListItem], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:list-block", element, attr: _*) <<|> content <<| "</fo:list-block>"
     
-  def block (element: Element, content: Seq[Span], attr: (String,String)*) = 
+  /** Renders an FO `block` element and the specified nested spans.
+   */
+  def block (element: Element, content: Seq[Span], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:block", element, attr: _*) << content << "</fo:block>"
     
-  def block (element: Element, attr: (String,String)*) = 
+  /** Renders an empty FO `block` element.
+   */
+  def block (element: Element, attr: (String,String)*): FOWriter = 
     this <<@/ ("fo:block", element, attr: _*)
 
-  def blockWithWS (element: Element, content: Seq[Span], attr: (String,String)*) = 
+  /** Renders an FO `block` element and the specified nested spans, preserving
+   *  all whitespace within the text elements of those spans.
+   */
+  def blockWithWS (element: Element, content: Seq[Span], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:block", element, attr: _*) <<< content << "</fo:block>"
   
-  def inline (element: Element, content: Seq[Span], attr: (String,String)*) = 
+  /** Renders an FO `inline` element and the specified nested spans.
+   */
+  def inline (element: Element, content: Seq[Span], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:inline", element, attr: _*) << content << "</fo:inline>"
     
-  def internalLink (element: Element, target: String, content: Seq[Span], attr: (String,String)*) = 
+  /** Renders an FO `basic-link` element for an internal target.
+   */
+  def internalLink (element: Element, target: String, content: Seq[Span], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:basic-link", element, (attr :+ ("internal-destination"->target)): _*) << content << "</fo:basic-link>"
   
-  def externalLink (element: Element, url: String, content: Seq[Span], attr: (String,String)*) = 
+  /** Renders an FO `basic-link` element for an external target.
+   */
+  def externalLink (element: Element, url: String, content: Seq[Span], attr: (String,String)*): FOWriter = 
     this <<@ ("fo:basic-link", element, (attr :+ ("external-destination"->url)): _*) << content << "</fo:basic-link>"
-    
-  def externalGraphic (element: Element, src: String) =
+  
+  /** Renders an FO `external-graphic` element.
+   */
+  def externalGraphic (element: Element, src: String): FOWriter =
     this <<@/ ("fo:external-graphic", element, "src"->src,
         "inline-progression-dimension.maximum"->"100%", 
         "content-width"->"scale-down-to-fit")
-        
-  def listItem (element: Element, label: Seq[Span], body: Seq[Block], attr: (String,String)*) = {
+   
+  /** Renders an FO `list-item` element with the specified label and body.
+   */
+  def listItem (element: Element, label: Seq[Span], body: Seq[Block], attr: (String,String)*): FOWriter = {
     val content = List(ListItemLabel(Paragraph(label)), ListItemBody(body))
     this <<@ ("fo:list-item", element, attr: _*) <<|> content <<| "</fo:list-item>"
   }
    
-  def listItemLabel (element: Element, content: Block, attr: (String,String)*) =
+  /** Renders an FO `list-item-label` element.
+   */
+  def listItemLabel (element: Element, content: Block, attr: (String,String)*): FOWriter =
     this <<@ ("fo:list-item-label", element, attr :+ ("end-indent"->"label-end()"): _*) <<|> List(content) <<| "</fo:list-item-label>"
   
-  def listItemBody (element: Element, content: Seq[Block], attr: (String,String)*) =
+  /** Renders an FO `list-item-body` element.
+   */
+  def listItemBody (element: Element, content: Seq[Block], attr: (String,String)*): FOWriter =
     this <<@ ("fo:list-item-body", element, attr :+ ("start-indent"->"body-start()"): _*) <<|> content <<| "</fo:list-item-body>"
   
-  def footnote (element: Element, label: String, body: Seq[Block], options: Options) = {
+  /** Renders an FO `footnote` element.
+   */
+  def footnote (element: Element, label: String, body: Seq[Block], options: Options): FOWriter = {
     val labelElement = Text(s"[$label]", Styles("footnote-label"))
     val bodyElements = body match {
       case Paragraph(content, opts) +: rest => Paragraph(labelElement +: Text(" ") +: content, opts) +: rest
@@ -111,63 +141,111 @@ class FOWriter (out: String => Unit,
     this <<@ ("fo:footnote",element,Nil:_*) <<|> List(labelElement, FootnoteBody(bodyElements, options)) <<| "</fo:footnote>"
   }
   
-  def text (element: Element, content: String, attr: (String,String)*) = {
+  /** Renders an FO `inline` element and the specified text. 
+   *  Renders only the text itself in case there are no
+   *  attributes associated with the text.
+   */
+  def text (element: Element, content: String, attr: (String,String)*): FOWriter = {
     val attrs = attributes("fo:inline",element,attr)
     if (hasAttributes(attrs)) this <<@ ("fo:inline", NoOpt, attrs: _*) <<& content << "</fo:inline>"
     else this <<& content
   }
     
-  def textWithWS (element: Element, content: String, attr: (String,String)*) = {
+  /** Renders an FO `inline` element and the specified text, preserving
+   *  all whitespace. Renders only the text itself in case there are no
+   *  attributes associated with the text.
+   */
+  def textWithWS (element: Element, content: String, attr: (String,String)*): FOWriter = {
     val attrs = attributes("fo:inline",element,attr)
     if (hasAttributes(attrs)) this <<@ ("fo:inline", NoOpt, attrs: _*) <<<& content << "</fo:inline>"
     else this <<<& content
   }
   
-  def textBlockWithWS (element: Element, content: String, attr: (String,String)*) = {
+  /** Renders an FO `block` element and the specified text, preserving
+   *  all whitespace. Renders only the text itself in case there are no
+   *  attributes associated with the text.
+   */
+  def textBlockWithWS (element: Element, content: String, attr: (String,String)*): FOWriter = {
     val attrs = attributes("fo:block",element,attr)
     if (hasAttributes(attrs)) this <<@ ("fo:block", NoOpt, attrs: _*) <<<& content << "</fo:block>"
     else this <<<& content
   }
     
-  def rawText (element: Element, content: String, attr: (String,String)*) = {
+  /** Renders an FO `inline` element and the specified text, treating it as
+   *  "raw", prerendered XSL-FO output. Renders only the text itself in case there are no
+   *  attributes associated with the text.
+   */
+  def rawText (element: Element, content: String, attr: (String,String)*): FOWriter = {
     val attrs = attributes("fo:inline",element,attr)
     if (hasAttributes(attrs)) this <<@ ("fo:inline", NoOpt, attrs: _*) << content << "</fo:inline>"
     else this << content
   }
   
-  def hasAttributes (attrs: Seq[(String,Any)]) = attrs.exists(_._2 != None)
+  private def hasAttributes (attrs: Seq[(String,Any)]): Boolean = attrs.exists(_._2 != None)
   
-  def bookmarkTree (tree: BookmarkTree) =
+  /** Renders an FO `bookmark-tree` element and all of its nested bookmarks.
+   */
+  def bookmarkTree (tree: BookmarkTree): FOWriter =
     this <<@ ("fo:bookmark-tree", tree, Nil:_*) <<|> tree.bookmarks << "</fo:bookmark-tree>"
   
-  def bookmark (bookmark: Bookmark) = {
+  /** Renders an FO `bookmark` element and all of its nested bookmarks.
+   */
+  def bookmark (bookmark: Bookmark): FOWriter = {
     val content = BookmarkTitle(bookmark.title) +: bookmark.children
     this <<@ ("fo:bookmark", bookmark, ("internal-destination" -> buildId(bookmark.path.absolute, bookmark.ref))) <<|> content << "</fo:bookmark>"
   }
   
-  def bookmarkTitle (title: BookmarkTitle) = {
+  /** Renders an FO `bookmark-title` element.
+   */
+  def bookmarkTitle (title: BookmarkTitle): FOWriter = {
     this <<@ ("fo:bookmark-title", title, Nil:_*) <<|> title.content << "</fo:bookmark-title>"
   }
   
  
 }
 
+/** Companion providing tree elements specific to the XSL-FO renderer. 
+ *  These are usually not part of the document AST produced by a parser,
+ *  but only inserted dynamically during the render process to drive
+ *  features specific to FO output. 
+ */
 object FOWriter {
   
+  /** A leader element.
+   */
   case class Leader (options: Options = NoOpt) extends Span 
   
+  /** An internal link to be rendered as a page number.
+   *  
+   *  @param ref the local reference to the target element
+   *  @param path the path of the target document containing the local reference
+   *  @param options optional render hints
+   */
   case class PageNumberCitation (ref: String, path: PathInfo, options: Options = NoOpt) extends Span
-  
+
+  /** A label for a list item, represented by a single Block element.
+   */
   case class ListItemLabel (content: Block, options: Options = NoOpt) extends Block
-  
+
+  /** The body of a list item containing a sequence of block elements.
+   */
   case class ListItemBody (content: Seq[Block], options: Options = NoOpt) extends Block with BlockContainer[ListItemBody]
-  
+
+  /** The body of a footnote containing a sequence of block elements.
+   */
   case class FootnoteBody (content: Seq[Block], options: Options = NoOpt) extends Block with BlockContainer[FootnoteBody]
   
+  /** An entire bookmark tree and its nested bookmarks,
+   *  the top level element for FO bookmarks.
+   */
   case class BookmarkTree (bookmarks: Seq[Bookmark], options: Options = NoOpt) extends Block
 
+  /** A single bookmark and its nested children.
+   */
   case class Bookmark (ref: String, path: PathInfo, title: String, children: Seq[Bookmark], options: Options = NoOpt) extends Block
   
+  /** A bookmark title. 
+   */
   case class BookmarkTitle (content: String, options: Options = NoOpt) extends Block with TextContainer
   
 }
