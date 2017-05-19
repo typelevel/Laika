typelevel/Laika

commit af42b74e5676f144da44e44e350351bd46612c95

refactor recursive span parsing to reduce coupling

core/src/main/scala/laika/parse/markdown/BlockParsers.scala
@@ -94,8 +94,8 @@ trait BlockParsers extends laika.parse.BlockParsers { self: InlineParsers =>
       else trimmed
     } 
     
-    ((anyOf('#') min 1 max 6) ^^ { _.length }) ~ (not(blankLine) ~> restOfLine) ^^ 
-      { case level ~ text => Header(level, consumeAllSpans.parseMarkup(stripDecoration(text))) }
+    ((anyOf('#') min 1 max 6) ^^ { _.length }) ~ (not(blankLine) ~> recursiveSpans(restOfLine ^^ stripDecoration)) ^^
+      { case level ~ spans => Header(level, spans) }
   }
   
   /** Parses a 1st or 2nd level Setext header. A first level header consists of the
@@ -105,9 +105,9 @@ trait BlockParsers extends laika.parse.BlockParsers { self: InlineParsers =>
    *  In contrast to several other Markdown parsers this parser requires a blank line
    *  before the header. 
    */
-  lazy val setextHeader: Parser[Header] = textLine ~ (anyOf('=').min(1) | anyOf('-').min(1)) <~ wsEol ^^ {
-    case text ~ decoration if decoration.head == '=' => Header(1, consumeAllSpans.parseMarkup(text))
-    case text ~ _                                    => Header(2, consumeAllSpans.parseMarkup(text))
+  lazy val setextHeader: Parser[Header] = recursiveSpans(textLine) ~ (anyOf('=').min(1) | anyOf('-').min(1)) <~ wsEol ^^ {
+    case spans ~ decoration if decoration.head == '=' => Header(1, spans)
+    case spans ~ _                                    => Header(2, spans)
   }
   
   /** Parses a horizontal rule, a line only decorated with three or more `'*'`, `'-'` or `'_'`
@@ -134,13 +134,12 @@ trait BlockParsers extends laika.parse.BlockParsers { self: InlineParsers =>
   lazy val nonRecursiveBlock: Parser[Block] =
     atxHeader | setextHeader | (insignificantSpaces ~> (literalBlock | rule )) | paragraph
 
-  def parseParagraph (lines: List[String]): Paragraph = Paragraph(consumeAllSpans.parseMarkup(linesToString(lines)))
-
   /** Parses a single paragraph. Everything between two blank lines that is not
    *  recognized as a special Markdown block type will be parsed as a regular paragraph.
    */
   val paragraph: Parser[Paragraph] =
-    not(bulletListItemStart | enumListItemStart) ~> ((not(blankLine) ~> restOfLine) +) ^^ parseParagraph
+    not(bulletListItemStart | enumListItemStart) ~>
+      recursiveSpans(((not(blankLine) ~> restOfLine) +) ^^ linesToString) ^^ { Paragraph(_) }
    
   /** Parses a single paragraph nested inside another block.
    *  Markdown allows nested lists without preceding blank lines,
@@ -150,7 +149,8 @@ trait BlockParsers extends laika.parse.BlockParsers { self: InlineParsers =>
    */
   val nestedParagraph: Parser[Block] = {
     val list: Parser[Block] = bulletList | enumList
-    ((((not(bulletListItemStart | enumListItemStart | blankLine) ~> restOfLine) +) ^^ parseParagraph)
+    val line = not(bulletListItemStart | enumListItemStart | blankLine) ~> restOfLine
+    ((recursiveSpans((line +) ^^ linesToString) ^^ { Paragraph(_) })
         ~ opt(not(blankLine) ~> list)) ^^ {
       case p ~ None => p
       case p ~ Some(list) => BlockSequence(p :: list :: Nil) // another special case of a "tight" list

core/src/main/scala/laika/parse/rst/BlockParsers.scala
@@ -86,21 +86,22 @@ trait BlockParsers extends laika.parse.BlockParsers
    *  recognized as a special reStructuredText block type will be parsed as a regular paragraph.
    */
   def paragraph: Parser[Paragraph] = 
-      ((not(blankLine) ~> restOfLine) +) ^^ { lines => Paragraph(parseInline(lines mkString "\n")) }
-  
+    recursiveSpans(mergeSpanLines((not(blankLine) ~> restOfLine) +)) ^^ { Paragraph(_) }
+
 
   /** Parses a section header with both overline and underline.
    * 
    *  See [[http://docutils.sourceforge.net/docs/ref/rst/restructuredtext.html#sections]].
    */
-  def headerWithOverline: Parser[Block] = {
-    (punctuationChar take 1) >> { start =>
-      val char = start.charAt(0)
-      anyOf(char) >> { deco =>
-        val len = deco.length + 1
-        (wsEol ~> (anyBut('\n') max len) <~
-         wsEol ~ (anyOf(char) take len) ~
-         wsEol) ^^ { title => DecoratedHeader(OverlineAndUnderline(char), parseInline(title.trim)) }
+  def headerWithOverline: Parser[Block] = (punctuationChar take 1) >> { start =>
+    val char = start.charAt(0)
+    anyOf(char) >> { deco =>
+      val len = deco.length + 1
+      val text = recursiveSpans((anyBut('\n') max len) ^^ (_.trim))
+      val decoLine = anyOf(char) take len
+
+      (wsEol ~> text <~ wsEol ~ decoLine ~ wsEol) ^^ {
+        title => DecoratedHeader(OverlineAndUnderline(char), title)
       }
     }
   }
@@ -114,8 +115,9 @@ trait BlockParsers extends laika.parse.BlockParsers
       val title = (char + rest).trim
       (punctuationChar take 1) >> { start =>
         val char = start.charAt(0)
-        ((anyOf(char) min (title.length - 1)) ~
-         wsEol) ^^ { _ => DecoratedHeader(Underline(char), parseInline(title)) }
+        withRecursiveSpanParser((anyOf(char) min (title.length - 1)) ~ wsEol) ^^ {
+          case (recParser, _) => DecoratedHeader(Underline(char), recParser(title))
+        }
       }
     }
   }
@@ -141,9 +143,7 @@ trait BlockParsers extends laika.parse.BlockParsers
     val attributionStart = "---" | "--" | '\u2014' // em dash
         
     def attribution (indent: Int) = (ws take indent) ~ attributionStart ~ (ws max 1) ~> 
-      indentedBlock(minIndent = indent, endsOnBlankLine = true) ^^ { block => 
-      parseInline(block.lines mkString "\n")
-    }
+      recursiveSpans(mergeIndentedLines(indentedBlock(minIndent = indent, endsOnBlankLine = true)))
       
     lookAhead(ws take 1) ~> withRecursiveBlockParser(indentedBlock(
         firstLineIndented = true, linePredicate = not(attributionStart))) >> {

core/src/main/scala/laika/directive/DirectiveParsers.scala
@@ -168,8 +168,8 @@ object DirectiveParsers {
     }
     
     lazy val templateDirectiveParser: Parser[TemplateSpan] = {
-      val contextRefOrNestedBraces = '{' -> (reference(TemplateContextReference(_)) | nestedBraces)
-      val bodyContent = wsOrNl ~ '{' ~> (withSource(spans(DelimitedBy('}'), spanParsers + contextRefOrNestedBraces)) ^^ (_._2.dropRight(1)))
+      val contextRefOrNestedBraces = Map('{' -> (reference(TemplateContextReference(_)) | nestedBraces))
+      val bodyContent = wsOrNl ~ '{' ~> (withSource(delimitedRecursiveSpans(DelimitedBy('}'), contextRefOrNestedBraces)) ^^ (_._2.dropRight(1)))
       withSource(directiveParser(bodyContent, includeStartChar = false)) ^^ { case (result, source) =>
         
         def createContext (parts: PartMap, docCursor: Option[DocumentCursor]): Templates.DirectiveContext = {
@@ -199,20 +199,21 @@ object DirectiveParsers {
     }
     
     lazy val spanDirectiveParser: Parser[Span] = {
-      val contextRefOrNestedBraces = '{' -> (reference(MarkupContextReference(_)) | nestedBraces)
-      val bodyContent = wsOrNl ~ '{' ~> (withSource(spans(DelimitedBy('}'), spanParsers + contextRefOrNestedBraces)) ^^ (_._2.dropRight(1)))
-      withSource(directiveParser(bodyContent, includeStartChar = false)) ^^ { case (result, source) => // TODO - optimization - parsed spans might be cached for DirectiveContext (applies for the template parser, too)
-        
-        def createContext (parts: PartMap, docCursor: Option[DocumentCursor]): Spans.DirectiveContext = {
-          new DirectiveContextBase(parts, docCursor) with Spans.DirectiveContext {
-            val parser = new Spans.Parser {
-              def apply (source: String) = parseInline(source)
+      val contextRefOrNestedBraces = Map('{' -> (reference(MarkupContextReference(_)) | nestedBraces))
+      val bodyContent = wsOrNl ~ '{' ~> (withSource(delimitedRecursiveSpans(DelimitedBy('}'), contextRefOrNestedBraces)) ^^ (_._2.dropRight(1)))
+      withRecursiveSpanParser(withSource(directiveParser(bodyContent, includeStartChar = false))) ^^ {
+        case (recParser, (result, source)) => // TODO - optimization - parsed spans might be cached for DirectiveContext (applies for the template parser, too)
+
+          def createContext (parts: PartMap, docCursor: Option[DocumentCursor]): Spans.DirectiveContext = {
+            new DirectiveContextBase(parts, docCursor) with Spans.DirectiveContext {
+              val parser = new Spans.Parser {
+                def apply (source: String) = recParser(source)
+              }
             }
           }
-        }
-        def invalid (msg: String) = InvalidSpan(SystemMessage(laika.tree.Elements.Error, msg), Literal("@"+source))
-        
-        applyDirective(Spans)(result, getSpanDirective, createContext, DirectiveSpan(_), invalid, "span")
+          def invalid (msg: String) = InvalidSpan(SystemMessage(laika.tree.Elements.Error, msg), Literal("@"+source))
+
+          applyDirective(Spans)(result, getSpanDirective, createContext, DirectiveSpan(_), invalid, "span")
       }
     }
     
@@ -234,14 +235,14 @@ object DirectiveParsers {
         val trimmed = (block.lines mkString "\n").trim
         Either.cond(trimmed.nonEmpty, trimmed, "empty body")
       }
-      withRecursiveBlockParser(withSource(directiveParser(bodyContent, includeStartChar = true))) ^^ {
-        case (recParser, (result, source)) =>
+      withRecursiveSpanParser(withRecursiveBlockParser(withSource(directiveParser(bodyContent, includeStartChar = true)))) ^^ {
+        case (recSpanParser, (recBlockParser, (result, source))) =>
         
           def createContext (parts: PartMap, docCursor: Option[DocumentCursor]): Blocks.DirectiveContext = {
             new DirectiveContextBase(parts, docCursor) with Blocks.DirectiveContext {
               val parser = new Blocks.Parser {
-                def apply (source: String): Seq[Block] = recParser(source)
-                def parseInline (source: String): Seq[Span] = BlockDirectives.this.parseInline(source)
+                def apply (source: String): Seq[Block] = recBlockParser(source)
+                def parseInline (source: String): Seq[Span] = recSpanParser(source)
               }
             }
           }

core/src/main/scala/laika/parse/InlineParsers.scala
@@ -16,15 +16,13 @@
 
 package laika.parse
 
-import laika.parse.core.markup.{EndDelimiter, InlineDelimiter, NestedDelimiter}
+import laika.parse.core.markup.{EndDelimiter, InlineDelimiter, NestedDelimiter, RecursiveSpanParsers}
 import laika.parse.core.text.{DelimitedBy, DelimitedText}
 import laika.parse.core._
 
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
-import laika.tree.Elements.Span
-import laika.tree.Elements.Text
-import laika.tree.Elements.NoOpt
+import laika.tree.Elements._
   
 /** A generic base trait for inline parsers. Provides base parsers that abstract
  *  aspects of inline parsing common to most lightweight markup languages.
@@ -41,7 +39,7 @@ import laika.tree.Elements.NoOpt
  *  
  *  @author Jens Halm
  */
-trait InlineParsers extends MarkupParsers {
+trait InlineParsers extends MarkupParsers with RecursiveSpanParsers {
   
 
   /** The mapping of markup start characters to their corresponding
@@ -55,7 +53,7 @@ trait InlineParsers extends MarkupParsers {
    *  offset pointing to the character after the one
    *  specified as the key for the mapping.
    */
-  final lazy val spanParsers: Map[Char,Parser[Span]] = prepareSpanParsers
+  private lazy val spanParsers: Map[Char,Parser[Span]] = prepareSpanParsers
   
   /** Extension hook for modifying the default span parser map.
    *  The default implementation returns the specified parser unchanged.
@@ -160,14 +158,6 @@ trait InlineParsers extends MarkupParsers {
   def spans (parser: => DelimitedText[String], spanParsers: => Map[Char, Parser[Span]]): Parser[List[Span]]
       = inline(parser, spanParsers, new SpanBuilder)
 
-  /** Parses a list of spans based on the specified span parsers.
-    *
-    *  @param spanParsers a mapping from the start character of a span to the corresponding parser for nested span elements
-    *  @return the resulting parser
-    */
-  def spans (spanParsers: => Map[Char, Parser[Span]]): Parser[List[Span]]
-      = inline(DelimitedBy.Undelimited, spanParsers, new SpanBuilder)
-
   /** Parses text based on the specified helper parsers.
     *
     *  @param parser the parser for the text of the current element
@@ -181,7 +171,7 @@ trait InlineParsers extends MarkupParsers {
    *  In the default implementation any character can be escaped.
    *  Sub-traits may override this parser to restrict the number of escapable characters.
    */
-  lazy val escapedChar: Parser[String] = any take 1
+  protected lazy val escapedChar: Parser[String] = any take 1
 
   /** Adds support for escape sequences to the specified text parser.
     *
@@ -200,39 +190,79 @@ trait InlineParsers extends MarkupParsers {
    */
   def escapedUntil (char: Char*): Parser[String] = escapedText(DelimitedBy(char:_*).nonEmpty)
 
-  /** Fully parses the input string and produces a list of spans.
-   *
-   *  This function is expected to always succeed, errors would be considered a bug
-   *  of this library, as the parsers treat all unknown or malformed markup as regular
-   *  text. Some parsers might additionally insert system message elements in case
-   *  of markup errors.
-   *
-   *  @param source the input to parse
-   *  @param spanParsers a mapping from the start character of a span to the corresponding parser
-   *  @return the result of the parser in form of a list of spans
-   */
-  def parseInline (source: String, spanParsers: Map[Char, Parser[Span]]): List[Span] = {
-    val p = new MarkupParser(inline(DelimitedBy.Undelimited, spanParsers, new SpanBuilder))
-    p.parseMarkup(source)
+
+  def mergeSpanLines (p: Parser[Seq[String]]): Parser[String] = p ^^ (_.mkString("\n"))
+
+
+  private def createRecursiveSpanParser (spanParsers: => Map[Char, Parser[Span]]): Parser[List[Span]]
+    = inline(DelimitedBy.Undelimited, spanParsers, new SpanBuilder)
+
+  private lazy val consumeAllSpans: Parser[List[Span]] = createRecursiveSpanParser(spanParsers)
+
+  def recursiveSpans (p: Parser[String]): Parser[List[Span]] = Parser { ctx =>
+    p.parse(ctx) match {
+      case Success(str, next) =>
+        consumeAllSpans.parse(str) match {
+          case Success(spans, _) => Success(processReverseSpans(spans), next)
+          case f: Failure => f
+        }
+      case f: Failure => f
+    }
   }
 
-  /** Fully parses the input string and produces a list of spans, using the
-   *  default span parsers returned by the `spanParsers` method.
-   * 
-   *  This function is expected to always succeed, errors would be considered a bug
-   *  of this library, as the parsers treat all unknown or malformed markup as regular
-   *  text. Some parsers might additionally insert system message elements in case
-   *  of markup errors.
-   *  
-   *  @param source the input to parse
-   *  @return the result of the parser in form of a list of spans
-   */
-  def parseInline (source: String): List[Span] = parseInline(source, spanParsers)
+  def recursiveSpans (p: Parser[String], additionalParsers: => Map[Char, Parser[Span]] = Map.empty): Parser[List[Span]] = Parser { ctx =>
+    lazy val spanParser = createRecursiveSpanParser(spanParsers ++ additionalParsers)
+    p.parse(ctx) match {
+      case Success(str, next) =>
+        spanParser.parse(str) match {
+          case Success(spans, _) => Success(processReverseSpans(spans), next)
+          case f: Failure => f
+        }
+      case f: Failure => f
+    }
+  }
 
+  def recursiveSpans: Parser[List[Span]] = consumeAllSpans
 
-  def consumeAllSpans (spanParsers: Map[Char, Parser[Span]]): MarkupParser[List[Span]] =
-    new MarkupParser(spans(spanParsers))
+  def delimitedRecursiveSpans (textParser: DelimitedText[String], additionalSpanParsers: => Map[Char, Parser[Span]]): Parser[List[Span]] =
+    spans(textParser, spanParsers ++ additionalSpanParsers)
 
-  lazy val consumeAllSpans: MarkupParser[List[Span]] = consumeAllSpans(spanParsers)
+  def delimitedRecursiveSpans (textParser: DelimitedText[String]): Parser[List[Span]] =
+    spans(textParser, spanParsers)
+
+  def withRecursiveSpanParser [T] (p: Parser[T]): Parser[(String => List[Span], T)] = Parser { ctx =>
+    // TODO - avoid duplication with `withRecursiveBlockParser`
+    p.parse(ctx) match {
+      case Success(res, next) =>
+        val recParser: String => List[Span] = { source: String =>
+          consumeAllSpans.parse(source) match {
+            case Success(spans, _) => spans
+            case Failure(msg, next) =>
+              val message = SystemMessage(Error, msg.message(next))
+              val fallback = Text(source)
+              List(InvalidSpan(message, fallback))
+          }
+        }
+        Success((recParser, res), next)
+      case f: Failure => f
+    }
+  }
+
+  private def processReverseSpans (spans: List[Span]): List[Span] = {
+    // TODO - solve this elsewhere and more efficiently
+    if (spans.isEmpty) spans
+    else {
+      val buffer = new ListBuffer[Span]
+      val last = (spans.head /: spans.tail) {
+        case (t @ Text(content,_), Reverse(len, target, fallback, _)) =>
+          if (content.length < len) { buffer += t; fallback }
+          else { buffer += Text(content.dropRight(len)); target }
+        case (prev, Reverse(_, _, fallback, _)) => buffer += prev; fallback
+        case (prev, current)                    => buffer += prev; current
+      }
+      buffer += last
+      buffer.toList
+    }
+  }
 
 }

core/src/main/scala/laika/parse/core/markup/RecursiveParsers.scala
@@ -32,6 +32,6 @@ trait RecursiveBlockParsers {
 
 trait RecursiveSpanParsers {
 
-  def recursiveSpans (p: Parser[String]): Parser[Seq[Span]]
+  def recursiveSpans (p: Parser[String]): Parser[List[Span]]
 
 }

core/src/main/scala/laika/parse/markdown/InlineParsers.scala
@@ -18,6 +18,7 @@ package laika.parse.markdown
 
 import laika.parse.core.Parser
 import laika.parse.core.text.DelimitedBy
+import laika.rewrite.TreeUtil
 import laika.tree.Elements._
 import laika.util.~
  
@@ -77,7 +78,7 @@ trait InlineParsers extends laika.parse.InlineParsers { self =>
    *  @param postCondition the parser that checks any post conditions after the end delimiter has been read
    */
   def span (start: Parser[Any], endDelim: String, postCondition: Parser[Any]): Parser[List[Span]]
-    = start ~> spans(DelimitedBy(endDelim).withPostCondition(postCondition), spanParsers)
+    = start ~> delimitedRecursiveSpans(DelimitedBy(endDelim).withPostCondition(postCondition))
   
   /** Parses a span enclosed by a single occurrence of the specified character.
    *  Recursively parses nested spans, too. 
@@ -120,7 +121,9 @@ trait InlineParsers extends laika.parse.InlineParsers { self =>
   
   
   private def normalizeId (id: String): String = id.toLowerCase.replaceAll("[\n ]+", " ")
-  
+
+  type RecParser = (String => List[Span])
+
   /** Parses a link, including nested spans in the link text.
    *  Recognizes both, an inline link `[text](url)` and a link reference `[text][id]`.
    */
@@ -132,12 +135,12 @@ trait InlineParsers extends laika.parse.InlineParsers { self =>
       else ref
     }
     
-    def linkInline (text: String, url: String, title: Option[String]) = ExternalLink(consumeAllSpans.parseMarkup(text), url, title)
-    def linkReference (text: String, id: String, suffix: String): Span = {
+    def linkInline (p: RecParser, text: String, url: String, title: Option[String]) = ExternalLink(p(text), url, title)
+    def linkReference (p: RecParser, text: String, id: String, suffix: String): Span = {
       /* Markdown's design comes with a few arbitrary and inconsistent choices for how to handle nesting of brackets. 
        * The logic here is constructed to make the official test suite pass, other edge cases might still yield unexpected results.
        * Users usually should not bother and simply escape brackets which are not meant to be markup. */
-      val ref = LinkReference(consumeAllSpans.parseMarkup(text), normalizeId(id), "[" + text + suffix)
+      val ref = LinkReference(p(text), normalizeId(id), "[" + text + suffix)
       if (text == id) unwrap(ref, suffix) else ref
     }
     
@@ -148,18 +151,24 @@ trait InlineParsers extends laika.parse.InlineParsers { self =>
    *  Recognizes both, an inline image `![text](url)` and an image reference `![text][id]`.
    */
   val image: Parser[Span] = {
-    def imageInline (text: String, uri: String, title: Option[String]) = Image(text, URI(uri), title)
-    def imageReference (text: String, id: String, postFix: String): Span = ImageReference(text, normalizeId(id), "![" + text + postFix)
+
+    def imageInline (p: RecParser, text: String, uri: String, title: Option[String]) =
+      Image(text, URI(uri), title)
+
+    def imageReference (p: RecParser, text: String, id: String, postFix: String): Span =
+      ImageReference(text, normalizeId(id), "![" + text + postFix)
      
     '[' ~> resource(imageInline, imageReference)
   }
+
+
   
   /** Helper function that abstracts the common parser logic of links and images.
    * 
    *  @param inline factory function for creating a new inline link or image based on the text, url and optional title parameters
    *  @param ref factory function for creating a new link or image reference based on the text and id parameters
    */
-  def resource (inline: (String, String, Option[String]) => Span, ref: (String, String, String) => Span): Parser[Span] = {
+  def resource (inline: (RecParser, String, String, Option[String]) => Span, ref: (RecParser, String, String, String) => Span): Parser[Span] = {
     
     val linktext = text(DelimitedBy(']'), Map('\\' -> escapedChar, '[' -> (DelimitedBy(']') ^^ { "[" + _ + "]" })))
 
@@ -170,17 +179,18 @@ trait InlineParsers extends laika.parse.InlineParsers { self =>
        self.text(DelimitedBy(')',' ','\t').keepDelimiter, Map('\\' -> escapedChar))
     
     val urlWithTitle = '(' ~> url ~ opt(title) <~ ws ~ ')' ^^ {  
-      case url ~ title => text:String => inline(text, url, title)  
+      case url ~ title => (recParser: RecParser, text:String) => inline(recParser, text, url, title)
     }
-    
-    val refId =    ws ~ opt(eol) ~ ('[' ~> escapedUntil(']')) ^^ { 
-      case ws ~ lb ~ id => text:String => ref(text, id,   "]"+ws+ lb.getOrElse("") +"["+id+"]") }
+    val refId =    ws ~ opt(eol) ~ ('[' ~> escapedUntil(']')) ^^ {
+      case ws ~ lb ~ id => (recParser: RecParser, text:String) =>
+        ref(recParser, text, id,   "]"+ws+ lb.getOrElse("") +"["+id+"]") }
     val refEmpty = ws ~ opt(eol) ~ "[]" ^^ { 
-      case ws ~ lb ~ _  => text:String => ref(text, text, "]"+ws+ lb.getOrElse("") +"[]") }
+      case ws ~ lb ~ _  => (recParser: RecParser, text:String) =>
+        ref(recParser, text, text, "]"+ws+ lb.getOrElse("") +"[]") }
   
-    linktext ~ opt(urlWithTitle | refEmpty | refId) ^^ {
-      case text ~ None    => ref(text, text, "]")
-      case text ~ Some(f) => f(text)  
+    withRecursiveSpanParser(linktext) ~ opt(urlWithTitle | refEmpty | refId) ^^ {
+      case (recParser, text) ~ None    => ref(recParser, text, text, "]")
+      case (recParser, text) ~ Some(f) => f(recParser, text)
     }
   }
   

core/src/main/scala/laika/parse/markdown/html/HTMLParsers.scala
@@ -110,20 +110,29 @@ trait HTMLParsers extends InlineParsers with BlockParsers {
   /** Parses an HTML element without the leading `'<'`, but including 
    *  all the nested HTML and Text elements.
    */
-  def htmlElement (nested: Map[Char,Parser[Span]]): Parser[HTMLElement] = htmlStartTag >> { 
-    tag => spans(htmlEndTag(tag.name), nested) ^^ {
+  lazy val htmlElement: Parser[HTMLElement] = htmlStartTag >> {
+    tag => spans(htmlEndTag(tag.name), htmlBlockParsers) ^^ {
       spans => HTMLElement(tag, spans) 
+    }
+  }
+
+  /** Parses an HTML element without the leading `'<'`, but including
+    * all the nested HTML and Text elements, as well as any nested Markdown spans.
+    */
+  lazy val htmlElementWithNestedMarkdown: Parser[HTMLElement] = htmlStartTag >> {
+    tag => delimitedRecursiveSpans(htmlEndTag(tag.name)) ^^ {
+      spans => HTMLElement(tag, spans)
     }
   }
   
   
   /** Parses any of the HTML span elements supported by this trait, plus standard markdown inside HTML elements.
    */
-  lazy val htmlSpanWithNestedMarkdown: Parser[HTMLSpan] = htmlComment | htmlEmptyElement | htmlElement(spanParsers) | htmlEndTag | htmlStartTag
+  lazy val htmlSpanWithNestedMarkdown: Parser[HTMLSpan] = htmlComment | htmlEmptyElement | htmlElementWithNestedMarkdown | htmlEndTag | htmlStartTag
   
   /** Parses any of the HTML span elements supported by this trait, but no standard markdown inside HTML elements.
    */
-  lazy val htmlSpan: Parser[HTMLSpan] = htmlComment | htmlEmptyElement | htmlElement(htmlBlockParsers) | htmlEndTag | htmlStartTag
+  lazy val htmlSpan: Parser[HTMLSpan] = htmlComment | htmlEmptyElement | htmlElement | htmlEndTag | htmlStartTag
   
   
   private def mkString (result: ~[Char,String]): String = result._1.toString + result._2

core/src/main/scala/laika/parse/rst/Directives.scala
@@ -156,7 +156,10 @@ object Directives {
       
     def argument [T](convert: String => Either[String,T] = {s:String => Right(s)}, 
                      withWS: Boolean = false): Result[T]
-    
+
+    def spanArgument: Result[Seq[Span]]
+    def optSpanArgument: Result[Option[Seq[Span]]]
+
     def optArgument [T](convert: String => Either[String,T] = {s:String => Right(s)}, 
                      withWS: Boolean = false): Result[Option[T]]
     
@@ -256,6 +259,10 @@ object Directives {
     def blockContent: DirectivePart[Seq[Block]] = part(_.blockContent)
 
     def spanContent: DirectivePart[Seq[Span]] = part(_.spanContent)
+
+    def spanArgument: DirectivePart[Seq[Span]] = part(_.spanArgument)
+
+    def optSpanArgument: DirectivePart[Option[Seq[Span]]] = part(_.optSpanArgument)
     
     /** Specifies that the body of the directive markup should get passed to the conversion function as a raw string.
      * 

commit 8742033088a1fc2f7df52cdddde03402d9b97dc1

avoid premature span parsing in md setext header - fixes perf regression   from commit af42b74

core/src/main/scala/laika/parse/markdown/BlockParsers.scala
@@ -111,9 +111,9 @@ class BlockParsers (recParsers: RecursiveParsers) {
    *  In contrast to several other Markdown parsers this parser requires a blank line
    *  before the header. 
    */
-  lazy val setextHeader: Parser[Header] = recursiveSpans(textLine) ~ (anyOf('=').min(1) | anyOf('-').min(1)) <~ wsEol ^^ {
-    case spans ~ decoration if decoration.head == '=' => Header(1, spans)
-    case spans ~ _                                    => Header(2, spans)
+  lazy val setextHeader: Parser[Header] = textLine ~ withRecursiveSpanParser(anyOf('=').min(1) | anyOf('-').min(1)) <~ wsEol ^^ {
+    case text ~ ((parser, decoration)) if decoration.head == '=' => Header(1, parser(text))
+    case text ~ ((parser, _))                                    => Header(2, parser(text))
   }
   
   /** Parses a horizontal rule, a line only decorated with three or more `'*'`, `'-'` or `'_'`