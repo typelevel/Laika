81c087de0bc33afa9ecd7df78bdac6f30bcf73bb
Jens Halm <halm@spicefactory.org>
2017-06-10 22:38:14 +0100

    extract Repeat parser - #54

---
+++ core/src/main/scala/laika/parse/core/combinator/Repeat.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.parse.core.combinator
+
+import laika.parse.core._
+
+import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
+
+/**
+  * @author Jens Halm
+  */
+class Repeat[+T] (parser: Parser[T]) extends Parser[List[T]] {
+
+  def parse (ctx: ParserContext): Parsed[List[T]] = {
+
+    val elems = new ListBuffer[T]
+
+    @tailrec
+    def rec (ctx: ParserContext): Parsed[List[T]] = parser.parse(ctx) match {
+      case Success(x, next) => elems += x; rec(next)
+      case _ => Success(elems.toList, ctx)
+    }
+
+    val res = rec(ctx)
+
+    Success(elems.toList, res.next)
+
+  }
+
+
+}
