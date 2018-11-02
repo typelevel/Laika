19a9f39651ee1b390a6d44601a3ae1da4fc8f6e7
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2018-11-02 23:15:28 +0000

    Replace use of parallel collections with custom executor - closes #70

--- core/src/main/scala/laika/api/Executor.scala
+++ core/src/main/scala/laika/api/Executor.scala
@@ -16,6 +16,11 @@
 
 package laika.api
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+
 /** Temporary abstraction for sequential or parallel execution of tasks.
   * Later versions will support a more flexible approach, possibly based on cats-effects.
   *
@@ -23,7 +28,53 @@ package laika.api
   */
 private[api] object Executor {
 
-  def execute[T](ops: Seq[() => T], parallel: Boolean): Seq[T] =
-    if (parallel) ops.par map (_()) seq else ops map (_())
+  type Batch[T] = Seq[() => T]
+
+  /** The default level of parallelism, corresponding to the number
+    * of CPUs.
+    */
+  lazy val defaultParallelism = Runtime.getRuntime.availableProcessors
+
+  /** The default minimum number of operations for parallel execution.
+    */
+  lazy val defaultParallelThreshold = Math.max(2, defaultParallelism / 2)
+
+  /** Executes the specified batch either sequentially or in parallel,
+    * depending on the boolean flag provided.
+    */
+  def execute[T] (ops: Batch[T], parallel: Boolean): Seq[T] =
+    if (parallel) execute(ops, defaultParallelism, defaultParallelThreshold)
+    else execute(ops, 1, Int.MaxValue)
+
+  /** Executes the specified batch either sequentially or in parallel,
+    * depending on the specified parameters.
+    *
+    * @param ops the operations to execute
+    * @param parallelism the number of batches to be executed in parallel, use 1 for sequential
+    *                    execution
+    * @param parallelThreshold the minimum number of operations required for parallel execution
+    * @return the result (synchronously) of the executed batch
+    */
+  def execute[T] (ops: Batch[T], parallelism: Int, parallelThreshold: Int): Seq[T] = {
+    if (parallelism == 1 || ops.size < parallelThreshold) ops map (_.apply)
+    else {
+      val batches = createBatches(ops, parallelism).map(batch => Future(batch map (_.apply)))
+      val result = Future.sequence(batches)
+      Await.result(result, 1.minute).flatten
+    }
+  }
+
+  /** Splits the specified operations into batches based on the given
+    * desired parallelism.
+    */
+  def createBatches[T] (ops: Seq[() => T], parallelism: Int): Seq[Batch[T]] = {
+    val mod = ops.size % parallelism
+    val loSize = ops.size / parallelism
+    val hiSize = loSize + 1
+    val (hi,lo) = ops.splitAt(mod * hiSize)
+    val hiBatch = if (mod > 0)    hi.grouped(hiSize) else Nil
+    val loBatch = if (loSize > 0) lo.grouped(loSize) else Nil
+    hiBatch.toSeq ++ loBatch.toSeq
+  }
 
 }
