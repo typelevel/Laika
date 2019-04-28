05cc99fb5a0bbf758c51e4a1414e84b59f2ca68e
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-04-28 18:10:42 +0100

    handle indentation properly in new TextFormatter base - #84

--- core/src/main/scala/laika/render/TextFormatter.scala
+++ core/src/main/scala/laika/render/TextFormatter.scala
@@ -22,29 +22,41 @@ import laika.ast._
  *  
  *  @param renderChild the function to use for rendering child elements
  *  @param elementStack the stack of parent elements of this formatter in recursive rendering
- *  @param indentLevel the level of indentation for this formatter
- *  @param indentItem the string to write for a single level of indentation
- *  @param newLine the new line character sequence 
+ *  @param indentation the level of indentation for this formatter
  * 
  *  @author Jens Halm
  */
 abstract class TextFormatter[Rep <: TextFormatter[Rep]] (renderChild: (Rep, Element) => String,
                                                          elementStack: Seq[Element],
-                                                         indentLevel: Int = 0,
-                                                         indentItem: String = "  ",
-                                                         newLineChar: String = "\n") { this: Rep =>
+                                                         indentation: Indentation) { this: Rep =>
   
   /** A newline character followed by whitespace matching the indentation level of this instance. 
     */
-  val newLine: String = newLineChar + (" " * indentLevel)
+  val newLine: String = indentation.newLine
   
   val parents: Seq[Element] = elementStack.tail
   
+  
   private def renderCurrentElement: String = renderChild(this, elementStack.last)
   
-  def withMinIndentation (minIndent: Int): Rep
+  private lazy val nextLevel: Rep = if (indentation == Indentation.none) this else withIndentation(indentation.nextLevel)
+
   
-  def withChild (element: Element): Rep
+  protected def withChild (element: Element): Rep
+
+  protected def withIndentation (newIndentation: Indentation): Rep
+  
+  
+  def indented (f: Rep => String): String = f(nextLevel)
+  
+  def withoutIndentation (f: Rep => String): String = f(withIndentation(Indentation.none))
+  
+  def withMinIndentation (minIndent: Int)(f: Rep => String): String = {
+    val newIndentation = if (indentation == Indentation.none || indentation.currentLevel >= minIndent) this 
+    else withIndentation(indentation.copy(currentLevel = minIndent))
+    f(newIndentation)
+  }
+  
   
   /** Renders the specified elements, all on the same line.
    */
@@ -63,8 +75,25 @@ abstract class TextFormatter[Rep <: TextFormatter[Rep]] (renderChild: (Rep, Elem
    *  each of them on a new line with the indentation increased one level to the right.
    */
   def indentedChildren (elements: Seq[Element]): String = {
-    val formatter = withMinIndentation(indentLevel + 1)
-    formatter.newLine + formatter.childPerLine(elements)
+    indented { fmt =>
+      fmt.newLine + fmt.childPerLine(elements)
+    }
   }
+  
+}
+
+case class Indentation (currentLevel: Int, numSpaces: Int) {
+
+  val newLine: String = "\n" + (" " * currentLevel)
+
+  lazy val nextLevel: Indentation = if (numSpaces == 0) this else copy(currentLevel = currentLevel + numSpaces)
+
+}
+
+object Indentation {
+  
+  val none: Indentation = Indentation(0,0)
+  
+  val default: Indentation = Indentation(0,2)
   
 }
