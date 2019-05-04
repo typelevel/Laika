3985067b409cafb34d4e1c5d28105cf623962b97
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-05-04 16:54:11 +0100

    add new HTMLRenderer - #84

---
+++ core/src/main/scala/laika/render/HTMLRenderer2.scala
@@ -0,0 +1,256 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.render
+
+import laika.ast._
+
+/** Default renderer implementation for the HTML output format.
+  *
+  * @author Jens Halm
+  */
+class HTMLRenderer2 (messageLevel: MessageLevel, fileSuffix: String = "html") {
+
+
+  def render (fmt: HTMLFormatter, element: Element): String = {
+
+    def include (msg: SystemMessage): Boolean = messageLevel <= msg.level
+
+    def noneIfDefault [T](actual: T, default: T): Option[String] = if (actual == default) None else Some(actual.toString)
+
+    def renderBlocks (tagName: String, options: Options, content: Seq[Block], attrs: (String,Any)*): String = content match {
+      case Seq(ss: SpanSequence)     => fmt.element(tagName, options, Seq(ss), attrs: _*)
+      case Seq(Paragraph(spans,opt)) => fmt.element(tagName, options, Seq(SpanSequence(spans,opt)), attrs: _*)
+      case other                     => fmt.indentedElement(tagName, options, other, attrs: _*)
+    }
+
+    def renderTable (table: Table): String = {
+      val children = List(table.caption,table.columns,table.head,table.body) filterNot (_.content.isEmpty)
+      fmt.indentedElement("table", table.options, children)
+    }
+
+    object WithFallback {
+      def unapply (value: Element): Option[Element] = value match {
+        case f: Fallback => Some(f.fallback)
+        case _ => None
+      }
+    }
+
+    def renderBlockContainer [T <: BlockContainer[T]](con: BlockContainer[T]): String = {
+
+      def toTable (label: String, content: Seq[Block], options: Options): Table = {
+        val left = Cell(BodyCell, List(SpanSequence(List(Text(s"[$label]")))))
+        val right = Cell(BodyCell, content)
+        val row = Row(List(left,right))
+        Table(TableHead(Nil), TableBody(List(row)), Caption(),
+          Columns.options(Styles("label"),NoOpt), options)
+      }
+
+      def quotedBlockContent (content: Seq[Block], attr: Seq[Span]): Seq[Block] =
+        if (attr.isEmpty) content
+        else content :+ Paragraph(attr, Styles("attribution"))
+
+      def figureContent (img: Span, caption: Seq[Span], legend: Seq[Block]): List[Block] =
+        List(SpanSequence(List(img)), Paragraph(caption, Styles("caption")), BlockSequence(legend, Styles("legend")))
+
+      con match {
+        case RootElement(content, _)          => fmt.childPerLine(content)
+        case EmbeddedRoot(content,indent,_)   => fmt.withMinIndentation(indent)(_.childPerLine(content))
+        case Section(header, content,_)       => fmt.childPerLine(header +: content)
+        case TitledBlock(title, content, opt) => fmt.indentedElement("div", opt, Paragraph(title,Styles("title")) +: content)
+        case QuotedBlock(content,attr,opt)    => renderBlocks("blockquote", opt, quotedBlockContent(content,attr))
+        case BulletListItem(content,_,opt)    => renderBlocks("li", opt, content)
+        case EnumListItem(content,_,_,opt)    => renderBlocks("li", opt, content)
+        case DefinitionListItem(term,defn,_)  => fmt.element("dt", NoOpt, term) + fmt.newLine + renderBlocks("dd", NoOpt, defn)
+        case LineBlock(content,opt)           => fmt.indentedElement("div", opt + Styles("line-block"), content)
+        case Figure(img,caption,legend,opt)   => fmt.indentedElement("div", opt + Styles("figure"), figureContent(img,caption,legend))
+
+        case Footnote(label,content,opt)      => renderTable(toTable(label,content,opt + Styles("footnote")))
+        case Citation(label,content,opt)      => renderTable(toTable(label,content,opt + Styles("citation")))
+
+        case WithFallback(fallback)           => fmt.child(fallback)
+        case c: Customizable                  => c match {
+          case BlockSequence(content, NoOpt)  => fmt.childPerLine(content) // this case could be standalone above, but triggers a compiler bug then
+          case _                              => fmt.indentedElement("div", c.options, c.content)
+        }
+        case unknown                          => fmt.indentedElement("div", NoOpt, unknown.content)
+      }
+    }
+
+    def renderSpanContainer [T <: SpanContainer[T]](con: SpanContainer[T]): String = {
+      
+      def codeStyles (language: String) = if (language.isEmpty) Styles("code") else Styles("code", language)
+      
+      def crossLinkRef (path: PathInfo, ref: String) = {
+        val target = path.relative.name.lastIndexOf(".") match {
+          case -1 => path.relative.toString
+          case i  => (path.relative.parent / (path.relative.name.take(i) + "." + fileSuffix)).toString
+        }
+        if (ref.isEmpty) target else s"$target#$ref"
+      }
+
+      con match {
+
+        case Paragraph(content,opt)         => fmt.element("p", opt, content)
+        case Emphasized(content,opt)        => fmt.element("em", opt, content)
+        case Strong(content,opt)            => fmt.element("strong", opt, content)
+        case Deleted(content,opt)           => fmt.element("del", opt, content)
+        case Inserted(content,opt)          => fmt.element("ins", opt, content)
+        case ParsedLiteralBlock(content,opt)=> fmt.rawElement("pre", opt, fmt.withoutIndentation(_.element("code", NoOpt, content)))
+        case CodeBlock(lang,content,opt)    => fmt.rawElement("pre", opt + codeStyles(lang), fmt.withoutIndentation(_.element("code", NoOpt, content)))
+        case Code(lang,content,opt)         => fmt.withoutIndentation(_.element("code", opt + codeStyles(lang), content))
+        case Line(content,opt)              => fmt.element("div", opt + Styles("line"), content)
+        case Title(content, opt)            => fmt.element("h1", opt, content)
+        case Header(level, content, opt)    => fmt.newLine + fmt.element("h"+level.toString, opt,content)
+
+        case ExternalLink(content, url, title, opt)     => fmt.element("a", opt, content, "href" -> url,       "title" -> title.map(fmt.text))
+        case InternalLink(content, ref, title, opt)     => fmt.element("a", opt, content, "href" -> ("#"+ref), "title" -> title.map(fmt.text))
+        case CrossLink(content, ref, path, title, opt)  => fmt.element("a", opt, content, "href" -> crossLinkRef(path, ref), "title" -> title.map(fmt.text))
+
+        case WithFallback(fallback)         => fmt.child(fallback)
+        case c: Customizable                => c match {
+          case SpanSequence(content, NoOpt) => fmt.children(content) // this case could be standalone above, but triggers a compiler bug then
+          case _                            => fmt.element("span", c.options, c.content)
+        }
+        case unknown                        => fmt.element("span", NoOpt, unknown.content)
+      }
+    }
+
+    def renderTemplateSpanContainer [T <: TemplateSpanContainer[T]](con: TemplateSpanContainer[T]): String = {
+      con match {
+        case TemplateRoot(content, NoOpt)         => fmt.children(content)
+        case TemplateSpanSequence(content, NoOpt) => fmt.children(content)
+        case c: Customizable                      => fmt.element("span", c.options, c.content)
+        case unknown                              => fmt.element("span", NoOpt, unknown.content)
+      }
+    }
+
+    def renderListContainer [T <: ListContainer[T]](con: ListContainer[T]): String = con match {
+      case EnumList(content,format,start,opt) =>
+        fmt.indentedElement("ol", opt, content, "class" -> format.enumType.toString.toLowerCase, "start" -> noneIfDefault(start,1))
+      case BulletList(content,_,opt)   => fmt.indentedElement("ul", opt, content)
+      case DefinitionList(content,opt) => fmt.indentedElement("dl", opt, content)
+
+      case WithFallback(fallback)      => fmt.child(fallback)
+      case c: Customizable             => fmt.indentedElement("div", c.options, c.content)
+      case unknown                     => fmt.indentedElement("div", NoOpt, unknown.content)
+    }
+
+    def renderTextContainer (con: TextContainer): String = con match {
+      case Text(content,opt)           => opt match {
+        case NoOpt                     => fmt.text(content)
+        case _                         => fmt.textElement("span", opt, content)
+      }
+      case TemplateString(content,opt) => opt match {
+        case NoOpt                     => content
+        case _                         => fmt.rawElement("span", opt, content)
+      }
+      case RawContent(formats, content, opt) => if (formats.contains("html") || formats.contains("html")) { opt match {
+        case NoOpt                     => content
+        case _                         => fmt.rawElement("span", opt, content)
+      }} else ""
+      case Literal(content,opt)        => fmt.withoutIndentation(_.textElement("code", opt, content))
+      case LiteralBlock(content,opt)   => fmt.element("pre", opt, Seq(Literal(content)))
+      case Comment(content,_)          => fmt.comment(content)
+      case sn@ SectionNumber(_, opt)   => fmt.child(Text(sn.content, opt + Styles("sectionNumber")))
+
+      case WithFallback(fallback)      => fmt.child(fallback)
+      case c: Customizable             => fmt.textElement("span", c.options, c.content)
+      case unknown                     => fmt.text(unknown.content)
+    }
+
+    def renderSimpleBlock (block: Block): String = block match {
+      case Rule(opt)                   => fmt.emptyElement("hr", opt)
+      case InternalLinkTarget(opt)     => fmt.textElement("a", opt, "")
+      case TargetFormat("html",e,_)    => fmt.child(e)
+      case TargetFormat("xhtml",e,_)   => fmt.child(e)
+
+      case WithFallback(fallback)      => fmt.child(fallback)
+      case _                           => ""
+    }
+
+    def renderSimpleSpan (span: Span): String = span match {
+      case CitationLink(ref,label,opt) => fmt.textElement("a", opt + Styles("citation"), s"[$label]", "href"->("#"+ref))
+      case FootnoteLink(ref,label,opt) => fmt.textElement("a", opt + Styles("footnote"), s"[$label]", "href"->("#"+ref))
+
+      case Image(text,uri,width,height,title,opt) =>
+        def sizeAttr (size: Option[Size], styleName: String): (Option[String],Option[String]) = size map {
+          case Size(amount, "px") => (Some(amount.toInt.toString), None)
+          case Size(amount, unit) => (None, Some(s"$styleName:$amount$unit"))
+        } getOrElse (None, None)
+        val (widthAttr, wStyle) = sizeAttr(width, "width")
+        val (heightAttr, hStyle) = sizeAttr(height, "height")
+        val styleAttr = (wStyle ++ hStyle).reduceLeftOption((a,b) => s"$a;$b")
+        fmt.emptyElement("img", opt, "src"->uri.uri, "alt"->text, "title"->title,
+          "width"->widthAttr, "height"->heightAttr, "style"->styleAttr)
+
+      case LineBreak(_)                   => fmt.emptyElement("br")
+      case TemplateElement(elem,indent,_) => fmt.withMinIndentation(indent)(_.child(elem))
+
+      case WithFallback(fallback)         => fmt.child(fallback)
+      case _                              => ""
+    }
+
+    def renderTableElement (elem: TableElement): String = elem match {
+      case TableHead(rows,opt)         => fmt.indentedElement("thead", opt, rows)
+      case TableBody(rows,opt)         => fmt.indentedElement("tbody", opt, rows)
+      case Columns(columns,opt)        => fmt.indentedElement("colgroup", opt, columns)
+      case Row(cells,opt)              => fmt.indentedElement("tr", opt, cells)
+      case Caption(content, opt)       => fmt.element("caption", opt, content)
+      case Column(opt)                 => fmt.textElement("col", opt, "")
+      case Cell(HeadCell, content, colspan, rowspan, opt) =>
+        renderBlocks("th", opt, content, "colspan" -> noneIfDefault(colspan,1), "rowspan" -> noneIfDefault(rowspan,1))
+      case Cell(BodyCell, content, colspan, rowspan, opt) =>
+        renderBlocks("td", opt, content, "colspan" -> noneIfDefault(colspan,1), "rowspan" -> noneIfDefault(rowspan,1))
+    }
+
+    def renderUnresolvedReference (ref: Reference): String =
+      fmt.child(InvalidElement(s"unresolved reference: $ref", ref.source).asSpan)
+
+    def renderInvalidElement (elem: Invalid[_ <: Element]): String = elem match {
+      case InvalidBlock(msg, fallback, opt) => 
+        if (include(msg)) fmt.children(List(Paragraph(List(msg),opt), fallback))
+        else fmt.child(fallback)
+      case e => 
+        if (include(e.message)) fmt.child(e.message) + " " + fmt.child(e.fallback)
+        else fmt.child(e.fallback)
+    }
+
+    def renderSystemMessage (message: SystemMessage): String = {
+      if (include(message))
+        fmt.textElement("span", message.options + Styles("system-message", message.level.toString.toLowerCase), message.content)
+      else ""
+    }
+
+    element match {
+      case e: SystemMessage       => renderSystemMessage(e)
+      case e: Table               => renderTable(e)
+      case e: TableElement        => renderTableElement(e)
+      case e: Reference           => renderUnresolvedReference(e)
+      case e: Invalid[_]          => renderInvalidElement(e)
+      case e: BlockContainer[_]   => renderBlockContainer(e)
+      case e: SpanContainer[_]    => renderSpanContainer(e)
+      case e: ListContainer[_]    => renderListContainer(e)
+      case e: TextContainer       => renderTextContainer(e)
+      case e: TemplateSpanContainer[_] => renderTemplateSpanContainer(e)
+      case e: Block               => renderSimpleBlock(e)
+      case e: Span                => renderSimpleSpan(e)
+
+      case _                      => ""
+    }
+  }
+
+}
