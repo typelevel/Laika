86f46cc9b84b52fce14ad20e7e95f3e8346f640b
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-05-21 21:47:29 +0100

    add new pure MarkupParser - #88

---
+++ core/src/main/scala/laika/api/MarkupParser.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.api
+
+import laika.ast.{Document, Path}
+import laika.ast.Path.Root
+import laika.config.OperationConfig
+import laika.parse.ParserContext
+import laika.parse.markup.DocumentParser
+import laika.parse.markup.DocumentParser.ParserInput
+
+class MarkupParser (parser: laika.factory.MarkupParser, val config: OperationConfig, rewrite: Boolean) {
+
+  private val docParser = DocumentParser.forMarkup(parser, config.markupExtensions, config.configHeaderParser)
+
+  def parse (input: String): Document = parse(input, Root)
+
+  def parse (input: String, path: Path): Document = {
+    val parserInput = ParserInput(path, ParserContext(input))
+    val doc = docParser(parserInput)
+    if (rewrite) doc.rewrite(config.rewriteRulesFor(doc))
+    else doc
+  }
+
+}
+
+/** Serves as an entry point to the Parse API.
+  *
+  *  @author Jens Halm
+  */
+object MarkupParser {
+
+  /** Returns a new Parse instance for the specified parser factory.
+    *  This factory is usually an object provided by the library
+    *  or a plugin that is capable of parsing a specific markup
+    *  format like Markdown or reStructuredText. 
+    *
+    *  @param parser the parser factory to use for all subsequent operations
+    */
+  def as (parser: laika.factory.MarkupParser): Parse = new Parse(
+    parser,
+    OperationConfig.default.withBundlesFor(parser),
+    rewrite = true
+  )
+
+}
