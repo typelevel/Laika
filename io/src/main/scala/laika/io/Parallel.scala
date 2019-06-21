0ee1272f1224cce0003a5d76df8ef7652715fa81
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-06-21 03:27:02 +0100

    scaladoc for new laika-io API entry points - #88

--- io/src/main/scala/laika/io/Parallel.scala
+++ io/src/main/scala/laika/io/Parallel.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package laika.io
 
 import cats.data.NonEmptyList
@@ -5,20 +21,309 @@ import laika.api.builder._
 import laika.api.{MarkupParser, Renderer, Transformer}
 import laika.factory.BinaryPostProcessor
 
+/** Entry point of the builder API for all parser, renderer and transform operations
+  * that process an entire tree of inputs and/or outputs.
+  *
+  * It builds on top of the parsers, renderers and transformers from the laika-core module
+  * and wraps support for file/stream IO and binary output around them.
+  * 
+  * This type of parser, renderer and transformer is the most powerful of the library, and the only
+  * option that supports templating, style sheets, copying of static files from input to output directory
+  * and parallel processing.
+  * 
+  * This API can be used to produce an entire HTML site or e-books in the EPUB or PDF format.
+  * It is also the basis of all tasks in the sbt plugin, that only adds a thin layer of sbt tasks
+  * and settings on top of the library API.
+  * 
+  * The tree for all internal processing steps is based on a virtual path, where in case
+  * of directory input and output the root of the virtual tree is mapped to the directory.
+  * This abstraction allows the merging of multiple input directories or the addition
+  * of inputs constructed in memory to those obtained from the file system.
+  * 
+  * In the case of binary output like EPUB or PDF, the document tree will be rendered
+  * to a single, merged output. In case of HTML the output is a tree of separate HTML documents.
+  * The API adjusts to the format in use, e.g. EPUB and PDF transformers will offer a
+  * `fromDirectory` ... `toFile` flow whereas HTML will offer `fromDirectory` ... `toDirectory`.
+  *
+  * Example for transforming a directory of Markdown files to a directory of HTML files:
+  *
+  * {{{
+  * implicit val processingContext: ContextShift[IO] = 
+  *   IO.contextShift(ExecutionContext.global)
+  *
+  * val blockingContext: ContextShift[IO] = 
+  *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+  *
+  * val transformer = Transformer
+  *   .from(Markdown)
+  *   .to(HTML)
+  *   .using(GitHubFlavor)
+  *
+  * val res: IO[Unit] = laika.io.Parallel(transformer)
+  *   .build(processingContext, blockingContext)
+  *   .fromDirectory("src")
+  *   .toDirectory("target")
+  *   .transform  
+  * }}}
+  *
+  * The first transformer built in the example in the pure transformer from the laika-core module
+  * that specifies the markup and render format and other configuration options and extensions.
+  *
+  * The `laika.io.Parallel` entry point then adds IO capabilities to the existing parser.
+  * It is based on an abstract effect from the cats-effect library and thus can be used with
+  * cats IO, Monix or Zio. The example above shows the usage with cats IO.
+  */
 object Parallel {
 
-  def apply (parser: ParserBuilder): text.ParallelParser.Builder                   = text.ParallelParser.Builder(NonEmptyList.of(parser.build))
-  def apply (renderer: RendererBuilder[_]): text.ParallelRenderer.Builder          = text.ParallelRenderer.Builder(renderer.build)
-  def apply (transformer: TransformerBuilder[_]): text.ParallelTransformer.Builder = text.ParallelTransformer.Builder(transformer.build)
+  /** Entry point of the builder API for a parsing operation for tree of inputs.
+    *
+    * Example for parsing a directory of Markdown files:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val parser = Parser
+    *   .of(Markdown)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[DocumentTreeRoot] = laika.io.Parallel(parser)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .parse  
+    * }}}
+    */
+  def apply (parser: ParserBuilder): text.ParallelParser.Builder = 
+    text.ParallelParser.Builder(NonEmptyList.of(parser.build))
 
-  def apply (renderer: TwoPhaseRendererBuilder[_, BinaryPostProcessor]): binary.ParallelRenderer.Builder          = binary.ParallelRenderer.Builder(renderer.build)
-  def apply (transformer: TwoPhaseTransformerBuilder[_, BinaryPostProcessor]): binary.ParallelTransformer.Builder = binary.ParallelTransformer.Builder(transformer.build)
+  /** Entry point of the builder API for a rendering operation to a tree of character outputs.
+    *
+    * Example for rendering HTML files to a directory:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val tree: DocumentTreeRoot = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(HTML)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(tree)
+    *   .toDirectory("target")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: RendererBuilder[_]): text.ParallelRenderer.Builder = 
+    text.ParallelRenderer.Builder(renderer.build)
 
-  def apply (parser: MarkupParser): text.ParallelParser.Builder           = text.ParallelParser.Builder(NonEmptyList.of(parser))
-  def apply (renderer: Renderer): text.ParallelRenderer.Builder           = text.ParallelRenderer.Builder(renderer)
-  def apply (transformer: Transformer): text.ParallelTransformer.Builder  = text.ParallelTransformer.Builder(transformer)
+  /** Entry point of the builder API for a transformation from a tree of inputs to a tree of character outputs.
+    *
+    * Example for transforming a directory of Markdown files to HTML:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .toDirectory("target")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TransformerBuilder[_]): text.ParallelTransformer.Builder = 
+    text.ParallelTransformer.Builder(transformer.build)
 
-  def apply (renderer: TwoPhaseRenderer[BinaryPostProcessor]): binary.ParallelRenderer.Builder           = binary.ParallelRenderer.Builder(renderer)
-  def apply (transformer: TwoPhaseTransformer[BinaryPostProcessor]): binary.ParallelTransformer.Builder  = binary.ParallelTransformer.Builder(transformer)
+  /** Entry point of the builder API for a rendering operation that renders a document tree merged into a single binary 
+    * output.
+    *
+    * Example for rendering an EPUB file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val tree: DocumentTreeRoot = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(EPUB)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(tree)
+    *   .toFile("out.epub")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: TwoPhaseRendererBuilder[_, BinaryPostProcessor]): binary.ParallelRenderer.Builder = 
+    binary.ParallelRenderer.Builder(renderer.build)
+
+  /** Entry point of the builder API for a transformation from a tree of inputs merged into to a single binary output.
+    *
+    * Example for transforming a directory of Markdown files to an EPUB document:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .toFile("out.epub")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TwoPhaseTransformerBuilder[_, BinaryPostProcessor]): binary.ParallelTransformer.Builder = 
+    binary.ParallelTransformer.Builder(transformer.build)
+
+  /** Entry point of the builder API for a parsing operation for tree of inputs.
+    *
+    * Example for parsing a directory of Markdown files:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val parser = Parser
+    *   .of(Markdown)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[DocumentTreeRoot] = laika.io.Parallel(parser)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .parse  
+    * }}}
+    */
+  def apply (parser: MarkupParser): text.ParallelParser.Builder = text.ParallelParser.Builder(NonEmptyList.of(parser))
+
+  /** Entry point of the builder API for a rendering operation to a tree of character outputs.
+    *
+    * Example for rendering HTML files to a directory:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val tree: DocumentTreeRoot = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(HTML)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(tree)
+    *   .toDirectory("target")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: Renderer): text.ParallelRenderer.Builder = text.ParallelRenderer.Builder(renderer)
+
+  /** Entry point of the builder API for a transformation from a tree of inputs to a tree of character outputs.
+    *
+    * Example for transforming a directory of Markdown files to HTML:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .toDirectory("target")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: Transformer): text.ParallelTransformer.Builder = text.ParallelTransformer.Builder(transformer)
+
+  /** Entry point of the builder API for a rendering operation that renders a document tree merged into a single binary 
+    * output.
+    *
+    * Example for rendering an EPUB file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val tree: DocumentTreeRoot = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(EPUB)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(tree)
+    *   .toFile("out.epub")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: TwoPhaseRenderer[BinaryPostProcessor]): binary.ParallelRenderer.Builder = 
+    binary.ParallelRenderer.Builder(renderer)
+
+  /** Entry point of the builder API for a transformation from a tree of inputs merged into to a single binary output.
+    *
+    * Example for transforming a directory of Markdown files to an EPUB document:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Parallel(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromDirectory("src")
+    *   .toFile("out.epub")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TwoPhaseTransformer[BinaryPostProcessor]): binary.ParallelTransformer.Builder = 
+    binary.ParallelTransformer.Builder(transformer)
 
 }
--- io/src/main/scala/laika/io/Sequential.scala
+++ io/src/main/scala/laika/io/Sequential.scala
@@ -1,29 +1,308 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package laika.io
 
 import laika.api.builder._
 import laika.api.{MarkupParser, Renderer, Transformer}
 import laika.factory.BinaryPostProcessor
 
+/** Entry point of the builder API for all parser, renderer and transform operations
+  * that process a single input and/or output.
+  * 
+  * It builds on top of the parsers, renderers and transformers from the laika-core module
+  * and wraps support for file/stream IO and binary output around them.
+  * 
+  * Example for transforming a Markdown file to an HTML file:
+  * 
+  * {{{
+  * implicit val processingContext: ContextShift[IO] = 
+  *   IO.contextShift(ExecutionContext.global)
+  *
+  * val blockingContext: ContextShift[IO] = 
+  *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+  *   
+  * val transformer = Transformer
+  *   .from(Markdown)
+  *   .to(HTML)
+  *   .using(GitHubFlavor)
+  *
+  * val res: IO[Unit] = laika.io.Sequential(transformer)
+  *   .build(processingContext, blockingContext)
+  *   .fromFile("hello.md")
+  *   .toFile("hello.html")
+  *   .transform  
+  * }}}
+  * 
+  * The first transformer built in the example in the pure transformer from the laika-core module
+  * that specifies the markup and render format and other configuration options and extensions.
+  * 
+  * The `laika.io.Sequential` entry point then adds IO capabilities to the existing parser.
+  * It is based on an abstract effect from the cats-effect library and thus can be used with
+  * cats IO, Monix or Zio. The example above shows the usage with cats IO.
+  */
 object Sequential {
 
+  /** Entry point of the builder API for a parsing operation for a single input.
+    * 
+    * Example for parsing a Markdown file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val parser = Parser
+    *   .of(Markdown)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Document] = laika.io.Sequential(parser)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("hello.md")
+    *   .parse  
+    * }}}
+    */
   def apply (parser: ParserBuilder): text.SequentialParser.Builder = text.SequentialParser.Builder(parser.build)
 
-  def apply (renderer: RendererBuilder[_]): text.SequentialRenderer.Builder = text.SequentialRenderer.Builder(renderer.build)
+  /** Entry point of the builder API for a rendering operation for a single character output.
+    *
+    * Example for rendering an HTML file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val doc: Document = ??? // obtained from a previous parse operation or constructed in-memory.
+    * 
+    * val renderer = Renderer.of(HTML)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(doc)
+    *   .toFile("out.html")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: RendererBuilder[_]): text.SequentialRenderer.Builder = 
+    text.SequentialRenderer.Builder(renderer.build)
 
-  def apply (transformer: TransformerBuilder[_]): text.SequentialTransformer.Builder = text.SequentialTransformer.Builder(transformer.build)
+  /** Entry point of the builder API for a transformation from a single input to a single character output.
+    *
+    * Example for transforming a file from Markdown to HTML:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("input.md")
+    *   .toFile("output.html")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TransformerBuilder[_]): text.SequentialTransformer.Builder = 
+    text.SequentialTransformer.Builder(transformer.build)
 
-  def apply (renderer: TwoPhaseRendererBuilder[_, BinaryPostProcessor]): binary.SequentialRenderer.Builder = binary.SequentialRenderer.Builder(renderer.build)
+  /** Entry point of the builder API for a rendering operation for a single binary output.
+    *
+    * Example for rendering an EPUB file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val doc: Document = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(EPUB)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(doc)
+    *   .toFile("out.epub")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: TwoPhaseRendererBuilder[_, BinaryPostProcessor]): binary.SequentialRenderer.Builder = 
+    binary.SequentialRenderer.Builder(renderer.build)
 
-  def apply (transformer: TwoPhaseTransformerBuilder[_, BinaryPostProcessor]): binary.SequentialTransformer.Builder = binary.SequentialTransformer.Builder(transformer.build)
+  /** Entry point of the builder API for a transformation from a single input to a single binary output.
+    *
+    * Example for transforming a file from Markdown to EPUB:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(EPUB)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("input.md")
+    *   .toFile("output.epub")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TwoPhaseTransformerBuilder[_, BinaryPostProcessor]): binary.SequentialTransformer.Builder = 
+    binary.SequentialTransformer.Builder(transformer.build)
 
+  /** Entry point of the builder API for a parsing operation for a single input.
+    *
+    * Example for parsing a Markdown file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val parser = Parser
+    *   .of(Markdown)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Document] = laika.io.Sequential(parser)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("hello.md")
+    *   .parse  
+    * }}}
+    */
   def apply (parser: MarkupParser): text.SequentialParser.Builder = text.SequentialParser.Builder(parser)
 
+  /** Entry point of the builder API for a rendering operation for a single character output.
+    *
+    * Example for rendering an HTML file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val doc: Document = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(HTML)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(doc)
+    *   .toFile("out.html")
+    *   .render  
+    * }}}
+    */
   def apply (renderer: Renderer): text.SequentialRenderer.Builder = text.SequentialRenderer.Builder(renderer)
 
-  def apply (transformer: Transformer): text.SequentialTransformer.Builder = text.SequentialTransformer.Builder(transformer)
+  /** Entry point of the builder API for a transformation from a single input to a single character output.
+    *
+    * Example for transforming a file from Markdown to HTML:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(HTML)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("input.md")
+    *   .toFile("output.html")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: Transformer): text.SequentialTransformer.Builder = 
+    text.SequentialTransformer.Builder(transformer)
 
-  def apply (renderer: TwoPhaseRenderer[BinaryPostProcessor]): binary.SequentialRenderer.Builder = binary.SequentialRenderer.Builder(renderer)
+  /** Entry point of the builder API for a rendering operation for a single binary output.
+    *
+    * Example for rendering an EPUB file:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val doc: Document = ??? // obtained from a previous parse operation or constructed in-memory.
+    *
+    * val renderer = Renderer.of(EPUB)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(renderer)
+    *   .build(processingContext, blockingContext)
+    *   .from(doc)
+    *   .toFile("out.epub")
+    *   .render  
+    * }}}
+    */
+  def apply (renderer: TwoPhaseRenderer[BinaryPostProcessor]): binary.SequentialRenderer.Builder = 
+    binary.SequentialRenderer.Builder(renderer)
 
-  def apply (transformer: TwoPhaseTransformer[BinaryPostProcessor]): binary.SequentialTransformer.Builder = binary.SequentialTransformer.Builder(transformer)
+  /** Entry point of the builder API for a transformation from a single input to a single binary output.
+    *
+    * Example for transforming a file from Markdown to EPUB:
+    *
+    * {{{
+    * implicit val processingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.global)
+    *
+    * val blockingContext: ContextShift[IO] = 
+    *   IO.contextShift(ExecutionContext.fromExecutor(Executors.newCachedThreadPool()))  
+    *
+    * val transformer = Transformer
+    *   .from(Markdown)
+    *   .to(EPUB)
+    *   .using(GitHubFlavor)
+    *
+    * val res: IO[Unit] = laika.io.Sequential(transformer)
+    *   .build(processingContext, blockingContext)
+    *   .fromFile("input.md")
+    *   .toFile("output.epub")
+    *   .transform  
+    * }}}
+    */
+  def apply (transformer: TwoPhaseTransformer[BinaryPostProcessor]): binary.SequentialTransformer.Builder = 
+    binary.SequentialTransformer.Builder(transformer)
 
 }  
