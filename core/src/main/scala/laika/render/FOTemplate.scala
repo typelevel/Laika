980e93d1cec5e7990230e0448235c37504ef2fe8
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-10-29 23:43:48 +0000

    use the right config instance in FOTemplate

--- core/src/main/scala/laika/render/FOTemplate.scala
+++ core/src/main/scala/laika/render/FOTemplate.scala
@@ -73,7 +73,7 @@ object FOTemplate {
     private val coverImagePath = "pdf.coverImage"
 
     def resolve (cursor: DocumentCursor): Span = {
-      cursor.root.target.config // TODO - 0.13 - shouldn't we use cursor.config here?
+      cursor.config
         .get[String](coverImagePath)
         .toOption
         .fold[TemplateSpan](TemplateSpanSequence(Nil)) { coverPath =>
