5b7fb67c2fc61148fdf266fbc0f03e119998ad39
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2019-12-28 13:29:08 +0000

    resolve relative paths in IncludeHandler - #114

--- io/src/main/scala/laika/io/config/IncludeHandler.scala
+++ io/src/main/scala/laika/io/config/IncludeHandler.scala
@@ -16,12 +16,15 @@
 
 package laika.io.config
 
-import cats.implicits._
+import java.io.File
+import java.net.URL
+
 import cats.effect.Async
+import cats.implicits._
 import laika.config.Config.IncludeMap
 import laika.config.{ConfigParser, ConfigResourceError}
 import laika.io.runtime.Runtime
-import laika.parse.hocon.{IncludeAny, IncludeClassPath, IncludeFile, IncludeResource, IncludeUrl}
+import laika.parse.hocon.{IncludeAny, IncludeClassPath, IncludeFile, IncludeResource, IncludeUrl, ValidStringValue}
 
 /**
   * @author Jens Halm
@@ -35,13 +38,48 @@ object IncludeHandler {
     
     if (includes.isEmpty) Async[F].pure(Map.empty) else {
       
-      def prepareFile(include: IncludeFile, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure((include, include))
+      def prepareFile(include: IncludeFile, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure {
+        if (new File(include.resourceId.value).isAbsolute) (include, include)
+        else parent.flatMap {
+          case IncludeFile(id, _) => Option(new File(id.value).getParentFile)
+          case _ => None
+        } match {
+          case Some(parentFile) => (IncludeFile(ValidStringValue(new File(parentFile, include.resourceId.value).getPath), include.isRequired), include)
+          case None => (include, include)
+        }
+      }
       
-      def prepareClasspath(include: IncludeClassPath, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure((include, include))
+      def prepareClasspath(include: IncludeClassPath, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure {
+        if (include.resourceId.value.startsWith("/")) (include.copy(resourceId = ValidStringValue(include.resourceId.value.drop(1))), include)
+        else parent match {
+          case Some(p: IncludeClassPath) if p.resourceId.value.contains("/") => 
+            val parentPath = p.resourceId.value.substring(0, p.resourceId.value.lastIndexOf("/"))
+            val childPath = s"$parentPath/${include.resourceId.value}"
+            (IncludeClassPath(ValidStringValue(childPath), include.isRequired), include)
+          case _ => (include, include)
+        }
+      }
       
-      def prepareUrl(include: IncludeUrl, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure((include, include))
+      def prepareUrl(include: IncludeUrl, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].delay {
+        parent match {
+          case Some(p: IncludeUrl) => 
+            val parentUrl = new URL(p.resourceId.value)
+            val childUrl = new URL(parentUrl, include.resourceId.value)
+            (IncludeUrl(ValidStringValue(childUrl.toString), include.isRequired), include)
+          case _ => (include, include)
+        }
+      }
 
-      def prepareAny(include: IncludeAny, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = Async[F].pure((include, include))
+      def prepareAny(include: IncludeAny, parent: Option[IncludeResource]): F[(IncludeResource, IncludeResource)] = 
+        Async[F].delay(new URL(include.resourceId.value))
+          .flatMap(_ => prepareUrl(IncludeUrl(include.resourceId, include.isRequired), parent))
+          .handleErrorWith { _ =>
+            parent match {
+              case Some(_: IncludeClassPath) => prepareClasspath(IncludeClassPath(include.resourceId, include.isRequired), parent)
+              case Some(_: IncludeUrl)       => prepareUrl(IncludeUrl(include.resourceId, include.isRequired), parent)
+              case _                         => prepareFile(IncludeFile(include.resourceId, include.isRequired), parent)
+            }
+          }
       
       val preparedIncludes = includes.map {
         case RequestedInclude(i: IncludeFile, parent)      => prepareFile(i, parent)
@@ -64,13 +102,13 @@ object IncludeHandler {
         )
       }.flatMap { loadedResources =>
         val configParsers = loadedResources.unite.map(loaded => (loaded.resource, loaded.result.map(ConfigParser.parse)))
-        val recursiveIncludes = configParsers.flatMap { 
-          case (res, Right(parser)) => parser.includes.map(RequestedInclude(_, Some(res)))
-          case _ => Nil
-        }
         val includeMap = configParsers.map {
           case (resource, result) => (resource, result.flatMap(_.unresolved))
         }.toMap
+        val recursiveIncludes = configParsers.flatMap {
+          case (res, Right(parser)) => parser.includes.filterNot(includeMap.contains).map(RequestedInclude(_, Some(res)))
+          case _ => Nil
+        }
         load(recursiveIncludes).map(_ ++ includeMap)
       }
       
