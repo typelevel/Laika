59f5482d2502361513c30f33e7a8288d64393b49
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-01-15 19:12:04 +0000

    extract common base parsers for XML and HTML highlighters - #67

---
+++ core/src/main/scala/laika/parse/code/common/TagBasedFormats.scala
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.parse.code.common
+
+import laika.ast.~
+import laika.parse.Parser
+import laika.parse.code.languages.XML.{name, nameParser, stringWithEntities}
+import laika.parse.code.{CodeCategory, CodeSpan, CodeSpanParsers}
+import laika.parse.markup.InlineParsers
+import laika.parse.text.TextParsers.{delimitedBy, literal, success}
+
+/**
+  * @author Jens Halm
+  */
+trait TagBasedFormats {
+
+  import NumberLiteral._
+
+  val comment: CodeSpanParsers = Comment.multiLine("<!--", "-->")
+
+  private val id = Identifier.standard.withIdStartChars('_',':').withIdPartChars('-','.')
+  val nameParser: Parser[String] = id.standaloneParser.map(_.content)
+  def name(category: CodeCategory): CodeSpanParsers = id.withCategoryChooser(_ => category).build
+
+  val ref: CodeSpanParsers =
+    CodeSpanParsers(CodeCategory.EscapeSequence, '&') {
+      ("#x" ~ DigitParsers.hex.min(1) ~ ";").concat
+    } ++
+      CodeSpanParsers(CodeCategory.EscapeSequence, '&') {
+        ("#" ~ DigitParsers.decimal.min(1) ~ ";").concat
+      } ++
+      CodeSpanParsers(CodeCategory.Substitution, '&') {
+        (nameParser ~ ";").concat
+      }
+
+  val string: CodeSpanParsers = StringLiteral.singleLine('\'').build ++ StringLiteral.singleLine('"').build
+  val stringWithEntities: CodeSpanParsers = StringLiteral.singleLine('\'').embed(ref).build ++ StringLiteral.singleLine('"').embed(ref).build
+
+  case class TagParser(tagCategory: CodeCategory,
+                       start: String,
+                       end: String,
+                       tagName: Parser[String],
+                       embedded: Seq[CodeSpanParsers] = Nil) extends EmbeddedCodeSpans {
+
+    val category: CodeCategory = CodeCategory.XML.Punctuation
+
+    def embed(childSpans: CodeSpanParsers*): TagParser = {
+      copy(embedded = embedded ++ childSpans)
+    }
+
+    def build: CodeSpanParsers = CodeSpanParsers(start.head) {
+
+      def codeParser(p: Parser[String], category: CodeCategory): Parser[CodeSpan] = p.map(CodeSpan(_, category))
+
+      val contentParser = InlineParsers.spans(delimitedBy(end).keepDelimiter, spanParserMap).map(_.flatMap(toCodeSpans))
+      val startParser = codeParser(if (start.length > 1) literal(start.tail) else success(""), CodeCategory.XML.Punctuation)
+      val endParser = codeParser(literal(end), CodeCategory.XML.Punctuation)
+      val tagNameParser = codeParser(tagName, tagCategory)
+
+      (startParser ~ tagNameParser ~ contentParser ~ endParser).map {
+        case startPunct ~ tagNameSpan ~ content ~ endPunct =>
+          mergeCodeSpans(start.head, Seq(startPunct, tagNameSpan) ++ content :+ endPunct)
+      }
+    }
+
+  }
+
+  object TagParser {
+    def apply (tagCategory: CodeCategory,
+               start: String,
+               end: String,
+               tagName: String): TagParser =
+      new TagParser(tagCategory, start, end, literal(tagName))
+  }
+
+  val emptyTag: CodeSpanParsers = TagParser(CodeCategory.XML.TagName, "<", "/>", nameParser).embed(
+    stringWithEntities,
+    name(CodeCategory.AttributeName)
+  ).build
+
+  val startTag: CodeSpanParsers = TagParser(CodeCategory.XML.TagName, "<", ">", nameParser).embed(
+    stringWithEntities,
+    name(CodeCategory.AttributeName)
+  ).build
+
+  val endTag: CodeSpanParsers = TagParser(CodeCategory.XML.TagName, "</", ">", nameParser).build
+  
+}
