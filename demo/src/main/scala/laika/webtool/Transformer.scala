75dcd1ff454c14d93bb42f2499e99144ffd3b44b
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-02-15 18:05:25 +0000

    Demo App: implement new server routes

--- demo/src/main/scala/laika/webtool/Transformer.scala
+++ demo/src/main/scala/laika/webtool/Transformer.scala
@@ -19,22 +19,58 @@ package laika.webtool
 import io.circe.Json
 import io.circe.syntax._
 import laika.api.{MarkupParser, Renderer}
-import laika.ast.Document
+import laika.ast.{CodeBlock, Document, Path}
+import laika.bundle.SyntaxHighlighter
 import laika.factory.MarkupFormat
-import laika.format.HTML
+import laika.format.{AST, HTML, Markdown, ReStructuredText}
+import laika.markdown.github.GitHubFlavor
+import laika.parse.code.SyntaxHighlighting
+import laika.parse.code.languages.{HTMLSyntax, LaikaASTSyntax}
 import laika.parse.markup.DocumentParser.ParserError
 
 /**
   * @author Jens Halm
   */
 object Transformer {
+  
+  private lazy val parsers: Map[MarkupFormat, MarkupParser] = Map(
+    Markdown -> MarkupParser.of(Markdown).using(GitHubFlavor, SyntaxHighlighting).build,
+    ReStructuredText -> MarkupParser.of(ReStructuredText).using(SyntaxHighlighting).build
+  )
+  
+  private val htmlRenderer = Renderer.of(HTML).build
+  private val astRenderer = Renderer.of(AST).build
+
+  private def highlightAndRender (highlighter: SyntaxHighlighter, src: String): Either[ParserError, String] =
+    highlighter.rootParser
+      .parse(src).toEither
+      .left.map(msg => ParserError(msg, Path.Root))
+      .map(CodeBlock(highlighter.language.head, _))
+      .map(htmlRenderer.render)
+  
+  
+  def transformToRenderedHTML (format: MarkupFormat, input: String): Either[ParserError, String] =
+    parsers(format).parse(input).map(htmlRenderer.render)
+  
+  def transformToHTMLSource (format: MarkupFormat, input: String): Either[ParserError, String] =
+    transformToRenderedHTML(format, input).flatMap(highlightAndRender(HTMLSyntax, _))
+  
+  def transformToUnresolvedAST (format: MarkupFormat, input: String): Either[ParserError, String] =
+    parsers(format).parseUnresolved(input)
+      .map(r => astRenderer.render(r.document.content))
+      .flatMap(highlightAndRender(LaikaASTSyntax, _))
+  
+  def transformToResolvedAST (format: MarkupFormat, input: String): Either[ParserError, String] =
+    parsers(format).parse(input)
+      .map(r => astRenderer.render(r.content))
+      .flatMap(highlightAndRender(LaikaASTSyntax, _))
 
   /** Note that in application code the transformation is usually done
    *  in one line. Here we want to first obtain the raw document tree
    *  and then rewrite it manually (which is usually performed automatically)
    *  as we want to show both in the result.
    */
-  def transform (format: MarkupFormat, input: String): Either[ParserError, String] = {
+  def transform (format: MarkupFormat, input: String): Either[ParserError, String] = { // TODO - remove
 
     val parser = MarkupParser.of(format).build
     
--- demo/src/main/scala/laika/webtool/TransformerRoutes.scala
+++ demo/src/main/scala/laika/webtool/TransformerRoutes.scala
@@ -19,6 +19,7 @@ package laika.webtool
 import cats.effect.IO
 import laika.factory.MarkupFormat
 import laika.format.{Markdown, ReStructuredText}
+import laika.parse.markup.DocumentParser.ParserError
 import org.http4s.HttpRoutes
 import org.http4s._
 import org.http4s.dsl.io._
@@ -28,6 +29,26 @@ import org.http4s.headers.`Content-Type`
   * @author Jens Halm
   */
 object TransformerRoutes {
+  
+  object InputFormat {
+    def unapply(str: String): Option[MarkupFormat] = str match {
+      case "md" => Some(Markdown)
+      case "rst" => Some(ReStructuredText)
+      case _ => None
+    }
+  }
+  
+  type TransformFunction = (MarkupFormat, String) => Either[ParserError, String]
+
+  object OutputFormat {
+    def unapply(str: String): Option[TransformFunction] = str match {
+      case "html-rendered"  => Some(Transformer.transformToRenderedHTML)
+      case "html-source"    => Some(Transformer.transformToHTMLSource)
+      case "ast-resolved"   => Some(Transformer.transformToResolvedAST)
+      case "ast-unresolved" => Some(Transformer.transformToUnresolvedAST)
+      case _ => None
+    }
+  }
 
   private def transform(format: MarkupFormat, body: IO[String]): IO[Response[IO]] = for {
     input  <- body
@@ -35,10 +56,19 @@ object TransformerRoutes {
     resp   <- Ok(result)
   } yield resp.withContentType(`Content-Type`(MediaType.application.json))
 
+  private def transform(format: MarkupFormat, f: TransformFunction, body: IO[String]): IO[Response[IO]] = for {
+    input  <- body
+    result <- IO.fromEither(f(format, input))
+    resp   <- Ok(result)
+  } yield resp.withContentType(`Content-Type`(MediaType.text.html))
+
   val all: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
-    case req @ POST -> Root / "transform" / "md"  => transform(Markdown, req.as[String])
-    case req @ POST -> Root / "transform" / "rst" => transform(ReStructuredText, req.as[String])
+    case req @ POST -> Root / "transform" / "md"  => transform(Markdown, req.as[String]) // TODO - remove
+    case req @ POST -> Root / "transform" / "rst" => transform(ReStructuredText, req.as[String]) // TODO - remove
+
+    case req @ POST -> Root / "transform" / InputFormat(inFormat) / OutputFormat(outFormat) =>
+      transform(inFormat, outFormat, req.as[String])
     
   }
 
