b0999825d4385d8ded868fb38d27774c8fa2a69c
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-02-22 23:17:23 +0000

    add DateFormat implementation for Scala.js - #30

---
+++ core/js/src/main/scala/laika/time/PlatformDateFormatImpl.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.time
+
+import java.util.Date
+
+import scala.scalajs.js
+import scala.util.Try
+
+/**
+  * @author Jens Halm
+  */
+object PlatformDateFormatImpl extends PlatformDateFormat {
+
+  def parse (dateString: String): Either[String, Date] = {
+    Try(new Date(js.Date.parse(dateString).toLong)).toEither.left.map(_.getMessage)
+  }
+
+  // For now the js impl ignores the pattern, this might be enhanced in the future
+  private[laika] def format (date: Date, pattern: String): Either[String, String] =
+    Try(new js.Date(date.getTime.toDouble).toLocaleString).toEither.left.map(_.getMessage)
+  
+}
---
+++ core/js/src/test/scala/laika/time/PlatformDateFormatSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.time
+
+import java.util.Date
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.scalajs.js
+
+class PlatformDateFormatSpec extends AnyWordSpec with Matchers {
+
+  
+  private def getDate(dateString: String): Date = new Date(js.Date.parse(dateString).toLong)
+  
+  
+  "The parser of the DateFormat" should {
+    
+    "parse a date without time" in {
+      PlatformDateFormat.parse("2011-10-10") shouldBe Right(getDate("2011-10-10T00:00:00Z"))
+    }
+
+    "parse a local date time" in {
+      PlatformDateFormat.parse("2011-10-10T14:48:00") shouldBe Right(getDate("2011-10-10T14:48:00"))
+    }
+
+    "parse a UTC date time" in {
+      PlatformDateFormat.parse("2011-10-10T14:48:00Z") shouldBe Right(getDate("2011-10-10T14:48:00Z"))
+    }
+
+    "parse a date time with an offset" in {
+      PlatformDateFormat.parse("2011-10-10T14:48:00+0100") shouldBe Right(getDate("2011-10-10T13:48:00Z"))
+    }
+
+    "fail in case of invalid date format" in {
+      PlatformDateFormat.parse("2011-10-10XX14:48:00+0100").isLeft shouldBe true
+    }
+
+  }
+  
+  
+}
