c43bb134c01c93e98a97b7c830e9c8f33d661bb0
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-03-25 00:47:20 +0000

    add new LinkConfig for global link definitions - #129

---
+++ core/shared/src/main/scala/laika/rewrite/link/LinkConfig.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.rewrite.link
+
+import laika.ast.Target
+import laika.config._
+
+/**
+  * @author Jens Halm
+  */
+case class LinkConfig (targets: Seq[TargetDefinition])
+
+object LinkConfig {
+  
+  implicit val key: DefaultKey[LinkConfig] = DefaultKey("links")
+  
+  implicit val decoder: ConfigDecoder[LinkConfig] = {
+    case Traced(ov: ObjectValue, _) =>
+      val config = ov.toConfig
+      for {
+        targets <- config.get[Map[String, String]]("targets", Map.empty[String,String])
+      } yield {
+        val mappedTargets = targets.map {
+          case (id, targetURL) => TargetDefinition(id, Target.create(targetURL))
+        }
+        LinkConfig(mappedTargets.toSeq)
+      }
+
+    case Traced(invalid: ConfigValue, _) => Left(InvalidType("Object", invalid))
+  }
+  
+}
+
+case class TargetDefinition (id: String, target: Target)
+
