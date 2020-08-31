5d6c1fc11909d26cbb6649002a0654198e947d04
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-08-31 01:07:04 +0100

    helium: add spec for HTML head section in default EPUB template

---
+++ io/src/test/scala/laika/helium/HeliumEPUBHeadSpec.scala
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package laika.helium
+
+import cats.effect.IO
+import laika.api.Transformer
+import laika.ast.Path
+import laika.ast.Path.Root
+import laika.format.{EPUB, Markdown}
+import laika.io.helper.{InputBuilder, ResultExtractor, StringOps}
+import laika.io.implicits._
+import laika.io.model.StringTreeOutput
+import laika.io.{FileIO, IOFunSuite}
+import laika.theme._
+
+class HeliumEPUBHeadSpec extends IOFunSuite with InputBuilder with ResultExtractor with StringOps {
+
+  def transformer (theme: ThemeProvider) = Transformer
+    .from(Markdown)
+    .to(EPUB.XHTML)
+    .io(FileIO.blocker)
+    .parallel[IO]
+    .withTheme(theme)
+    .build
+  
+  val singleDoc = Seq(
+    Root / "name.md" -> "text"
+  )
+  
+  val defaultResult = """<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+                        |<meta charset="utf-8"/>
+                        |<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                        |<meta name="generator" content="Laika 0.16.0 + Helium Theme" />
+                        |<title></title>
+                        |<link rel="stylesheet" type="text/css" href="helium/laika-helium.epub.css" />
+                        |<script> /* for avoiding page load transitions */ </script>""".stripMargin
+  
+  def transformAndExtractHead(inputs: Seq[(Path, String)]): IO[String] = transformAndExtractHead(inputs, Helium.defaults)
+
+  def transformAndExtractHead(inputs: Seq[(Path, String)], helium: Helium): IO[String] = transformer(helium.build).use { t =>
+    for {
+      resultTree <- t.fromInput(build(inputs)).toOutput(StringTreeOutput).transform
+      res        <- IO.fromEither(resultTree.extractTidiedTagContent(Root / "name.epub.xhtml", "head")
+        .toRight(new RuntimeException("Missing document under test")))
+    } yield res
+  }
+
+  def transformAndExtract(inputs: Seq[(Path, String)], helium: Helium, start: String, end: String): IO[String] = transformer(helium.build).use { t =>
+    for {
+      resultTree <- t.fromInput(build(inputs)).toOutput(StringTreeOutput).transform
+      res        <- IO.fromEither(resultTree.extractTidiedSubstring(Root / "name.epub.xhtml", start, end)
+        .toRight(new RuntimeException("Missing document under test")))
+    } yield res
+  }
+    
+  test("Helium defaults via transformer") {
+    transformAndExtractHead(singleDoc).assertEquals(defaultResult)
+  }
+  
+  test("exclude CSS and JS from API directory") {
+    val inputs = Seq(
+      Root / "name.md" -> "text",
+      Root / "api" / "foo.js" -> "",
+      Root / "api" / "foo.css" -> "",
+    )
+    transformAndExtractHead(inputs).assertEquals(defaultResult)
+  }
+  
+  test("custom CSS and JS files") {
+    val inputs = Seq(
+      Root / "name.md" -> "text",
+      Root / "web" / "foo.js" -> "",
+      Root / "web" / "foo.shared.css" -> "",
+    )
+    val expected = """<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+                     |<meta charset="utf-8"/>
+                     |<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                     |<meta name="generator" content="Laika 0.16.0 + Helium Theme" />
+                     |<title></title>
+                     |<link rel="stylesheet" type="text/css" href="helium/laika-helium.epub.css" />
+                     |<link rel="stylesheet" type="text/css" href="web/foo.shared.css" />
+                     |<script src="web/foo.js"></script>
+                     |<script> /* for avoiding page load transitions */ </script>""".stripMargin
+    transformAndExtractHead(inputs).assertEquals(expected)
+  }
+
+  test("custom configuration for CSS and JS file locations") {
+    val inputs = Seq(
+      Root / "name.md" -> "text",
+      Root / "web" / "foo.js" -> "",
+      Root / "web" / "foo.shared.css" -> "",
+      Root / "custom-js" / "foo.js" -> "",
+      Root / "custom-css" / "foo.shared.css" -> "",
+    )
+    val helium = Helium.defaults
+      .epub.autoLinkCSS(Root / "custom-css")
+      .epub.autoLinkJS(Root / "custom-js")
+    val expected = """<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+                     |<meta charset="utf-8"/>
+                     |<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                     |<meta name="generator" content="Laika 0.16.0 + Helium Theme" />
+                     |<title></title>
+                     |<link rel="stylesheet" type="text/css" href="helium/laika-helium.epub.css" />
+                     |<link rel="stylesheet" type="text/css" href="custom-css/foo.shared.css" />
+                     |<script src="custom-js/foo.js"></script>
+                     |<script> /* for avoiding page load transitions */ </script>""".stripMargin
+    transformAndExtractHead(inputs, helium).assertEquals(expected)
+  }
+  
+  test("metadata (authors, description)") {
+    val helium = Helium.defaults.all.metadata(
+      authors = Seq("Maria Green", "Elena Blue"),
+      description = Some("Some description")
+    )
+    val expected = """<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+                     |<meta charset="utf-8"/>
+                     |<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                     |<meta name="generator" content="Laika 0.16.0 + Helium Theme" />
+                     |<title></title>
+                     |<meta name="author" content="Maria Green"/>
+                     |<meta name="author" content="Elena Blue"/>
+                     |<meta name="description" content="Some description"/>
+                     |<link rel="stylesheet" type="text/css" href="helium/laika-helium.epub.css" />
+                     |<script> /* for avoiding page load transitions */ </script>""".stripMargin
+    transformAndExtractHead(singleDoc, helium).assertEquals(expected)
+  }
+
+  test("metadata (language)") {
+    val helium = Helium.defaults.all.metadata(
+      language = Some("de")
+    )
+    transformAndExtract(singleDoc, helium, "<html ", ">").assertEquals("""lang="de" xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"""")
+  }
+
+  test("title") {
+    val markup =
+      """
+        |Title
+        |=====
+        |
+        |Text
+      """.stripMargin
+    val inputs = Seq(
+      Root / "name.md" -> markup
+    )
+    transformAndExtractHead(inputs).map(_.extractTag("title")).assertEquals(Some("Title"))
+  }
+  
+}
