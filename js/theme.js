e54c95a4862e134001b3014cf19ae72684096942
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2020-10-31 20:43:21 +0000

    manual for version 0.17.0

--- js/theme.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById("open-nav-icon").onclick = () => {
-    document.getElementById("sidebar").classList.add("nav-open");
-  };
-  document.getElementById("close-nav-icon").onclick = () => {
-    document.getElementById("sidebar").classList.remove("nav-open");
-  };
-});
