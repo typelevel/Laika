22740a9acc29a1c020180ac9582952000db7acdc
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2021-03-20 01:47:19 +0000

    CE3 - simplify Batch implementation

--- io/src/main/scala/laika/io/runtime/Batch.scala
+++ io/src/main/scala/laika/io/runtime/Batch.scala
@@ -18,7 +18,7 @@ package laika.io.runtime
 
 import cats.effect.Sync
 import cats.implicits._
-import cats.{Monad, Parallel, Traverse}
+import cats.Parallel
 
 /** Type class for the effect F that encapsulates the mechanism 
   * and configuration for executing a batch of operations, either sequentially or in parallel.
@@ -27,58 +27,35 @@ import cats.{Monad, Parallel, Traverse}
   */
 trait Batch[F[_]] {
 
-  /** The `Sync` instance for the type F.
+  /** Runs the specified batch, either sequentially or in parallel, depending on implementation.
     */
-  implicit def F: Sync[F]
-
-  /** The optional `Parallel` instance for the type F.
-    * 
-    * If it is missing all parallel invocation will downgrade to sequential execution.
-    */
-  def parallelInstance: Option[Parallel[F]]
-
-  /** The desired level of parallelism for all parallel operations.
-    */
-  def parallelism: Int
-
-  /** Runs the specified batch in parallel, but may fall back to sequential execution
-    * depending on configuration.
-    */
-  def execute[A] (fas: Vector[F[A]]): F[Vector[A]] = parallelInstance match {
-    case Some(par) if parallelism > 1 && fas.size > 1 => BatchRuntime
-      .createBatches(fas, parallelism)
-      .parSequence(implicitly[Monad[F]], implicitly[Traverse[Vector]], par)
-      .map(_.flatten)
-    case _ => fas.sequence
-  } 
-    
+  def execute[A] (fas: Vector[F[A]]): F[Vector[A]]
 
 }
 
-/** Constructors for Runtime instances for parallel or sequential execution.
+/** Constructors for Batch instances for parallel or sequential execution.
   */
 object Batch {
 
-  /** Summoner for implicit Runtime instances.
+  /** Summoner for implicit Batch instances.
     */
   def apply[F[_]: Batch]: Batch[F] = implicitly[Batch[F]]
 
-  /** Creates a Runtime instance for sequential execution based on the specified
-    * contexts for CPU-bound and blocking operations.
+  /** Creates a Batch instance for sequential execution.
     */
   def sequential[F[_]: Sync]: Batch[F] = new Batch[F] {
-    val F = implicitly[Sync[F]]
-    val parallelInstance = None
-    val parallelism = 1
+    def execute[A] (fas: Vector[F[A]]): F[Vector[A]] = fas.sequence
   }
 
-  /** Creates a Runtime instance for parallel execution based on the specified
-    * contexts for CPU-bound and blocking operations.
+  /** Creates a Batch instance for parallel execution.
     */
-  def parallel[F[_]: Sync: Parallel] (parallelismParam: Int): Batch[F] = new Batch[F] {
-    val F = implicitly[Sync[F]]
-    val parallelInstance = Some(implicitly[Parallel[F]])
-    val parallelism = parallelismParam
+  def parallel[F[_]: Sync: Parallel] (parallelism: Int): Batch[F] = new Batch[F] {
+    def execute[A] (fas: Vector[F[A]]): F[Vector[A]] = 
+      if (parallelism > 1 && fas.size > 1) BatchRuntime
+        .createBatches(fas, parallelism)
+        .parSequence
+        .map(_.flatten)
+      else fas.sequence
   }
 
 }
