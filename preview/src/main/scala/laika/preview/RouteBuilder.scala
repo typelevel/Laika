ee170e99a7b36bf37aa2aa52b5214a3f70dd0ad0
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2021-04-28 02:15:15 +0100

    preview server: integrate rendering of EPUB + PDF

--- preview/src/main/scala/laika/preview/RouteBuilder.scala
+++ preview/src/main/scala/laika/preview/RouteBuilder.scala
@@ -5,13 +5,12 @@ import java.io.InputStream
 import cats.syntax.all._
 import cats.effect.{Async, Resource, Sync}
 import fs2.io.readInputStream
-import laika.{ast, config}
 import org.http4s.EntityEncoder.entityBodyEncoder
 import org.http4s.dsl.Http4sDsl
 import org.http4s.{EntityEncoder, Headers, HttpRoutes, MediaType}
 import org.http4s.headers.`Content-Type`
 
-class RouteBuilder[F[_]: Async](cache: Cache[F, Map[ast.Path, Either[Resource[F, InputStream], String]]]) extends Http4sDsl[F] {
+class RouteBuilder[F[_]: Async](cache: Cache[F, SiteResults[F]]) extends Http4sDsl[F] {
 
   implicit def inputStreamResourceEncoder[G[_]: Sync, IS <: InputStream]: EntityEncoder[G, Resource[G, IS]] =
     entityBodyEncoder[G].contramap { (in: Resource[G, IS]) =>
@@ -28,10 +27,10 @@ class RouteBuilder[F[_]: Async](cache: Cache[F, Map[ast.Path, Either[Resource[F,
     case GET -> path =>
       val laikaPath = laika.ast.Path.parse(path.toString)
       cache.get.map(_.get(laikaPath.withoutFragment)).flatMap {
-        case Some(Right(content)) =>
+        case Some(RenderedResult(content)) =>
           Async[F].delay(println(s"serving path $laikaPath - transformed markup")) *>
           Ok(content).map(_.withContentType(`Content-Type`(MediaType.text.html)))
-        case Some(Left(input)) =>
+        case Some(StaticResult(input)) =>
           Async[F].delay(println(s"serving path $laikaPath - static input")) *> {
             val mediaType = laikaPath.suffix.flatMap(mediaTypeFor).map(`Content-Type`(_))
             Ok(input).map(_.withHeaders(Headers(mediaType)))
--- preview/src/main/scala/laika/preview/SiteTransformer.scala
+++ preview/src/main/scala/laika/preview/SiteTransformer.scala
@@ -16,65 +16,140 @@
 
 package laika.preview
 
-import java.io.InputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, InputStream}
 
 import cats.syntax.all._
 import cats.effect.{Async, Resource}
 import laika.api.Renderer
 import laika.api.builder.OperationConfig
-import laika.ast.Path
-import laika.format.HTML
-import laika.io.api.{TreeParser, TreeRenderer}
+import laika.ast.{DocumentTreeRoot, Path}
+import laika.config.Config.ConfigResult
+import laika.config.{ConfigException, LaikaKeys}
+import laika.factory.{BinaryPostProcessorBuilder, TwoPhaseRenderFormat}
+import laika.format.{EPUB, HTML, PDF}
+import laika.io.api.{BinaryTreeRenderer, TreeParser, TreeRenderer}
+import laika.io.config.SiteConfig
 import laika.io.implicits._
-import laika.io.model.{InputTreeBuilder, StringTreeOutput}
+import laika.io.model.{InputTreeBuilder, ParsedTree, StringTreeOutput}
+import laika.rewrite.nav.Selections
 import laika.theme.ThemeProvider
 
-class SiteTransformer[F[_]: Async] (val parser: TreeParser[F], htmlRenderer: TreeRenderer[F], inputs: InputTreeBuilder[F]) {
+class SiteTransformer[F[_]: Async] (val parser: TreeParser[F], 
+                                    htmlRenderer: TreeRenderer[F],
+                                    epubRenderer: BinaryTreeRenderer[F],
+                                    pdfRenderer: BinaryTreeRenderer[F],
+                                    inputs: InputTreeBuilder[F],
+                                    artifactBasename: String) {
 
-  private val tree = {
+  private val parse = {
     //        val apiPath = validated(SiteConfig.apiPath(baseConfig))
     //        val inputs = generateAPI.value.foldLeft(laikaInputs.value.delegate) {
     //          (inputs, path) => inputs.addProvidedPath(apiPath / path)
     //        }
     parser.fromInput(inputs).parse
   }
+  
+  def renderBinary (renderer: BinaryTreeRenderer[F], root: DocumentTreeRoot): Resource[F, InputStream] = {
+    val renderResult = for {
+      out <- Async[F].delay(new ByteArrayOutputStream)
+      _   <- renderer.from(root).toStream(Async[F].pure(out)).render
+    } yield out.toByteArray
+    
+    Resource
+      .eval(renderResult)
+      .flatMap(bytes => Resource.eval(Async[F].delay(new ByteArrayInputStream(bytes))))
+  }
+  
+  def transformBinaries (root: DocumentTreeRoot): ConfigResult[Map[Path, SiteResult[F]]] = {
+    for {
+      roots            <- Selections.createCombinations(root)
+      downloadPath     <- SiteConfig.downloadPath(root.config)
+      artifactBaseName <- root.config.get[String](LaikaKeys.artifactBaseName, artifactBasename)
+    } yield {
+      val combinations = 
+        roots.map(r => (r._1, r._2, epubRenderer, ".epub")) ++
+        roots.map(r => (r._1, r._2, pdfRenderer, ".pdf"))
+      combinations.map { case (root, classifiers, renderer, suffix) =>
+        val classifier = if (classifiers.value.isEmpty) "" else "-" + classifiers.value.mkString("-")
+        val docName = artifactBaseName + classifier + suffix
+        val path = downloadPath / docName
+        (path, StaticResult(renderBinary(renderer, root)))
+      }.toList.toMap
+    }
+  }
 
-  val transform: F[Map[Path, Either[Resource[F, InputStream], String]]] = tree.flatMap { tree =>
+  def transformHTML (tree: ParsedTree[F]): F[Map[Path, SiteResult[F]]] = {
     htmlRenderer
       .from(tree.root)
       .copying(tree.staticDocuments)
       .toOutput(StringTreeOutput)
       .render
       .map { root =>
-        (root.allDocuments.map { doc =>
-          (doc.path, Right(doc.content))
-        } ++
-          root.staticDocuments.map { doc =>
-            (doc.path, Left(doc.input))
-          })
-          .toMap // TODO - map root
+        val map = root.allDocuments.map { doc =>
+          (doc.path, RenderedResult[F](doc.content))
+        }.toMap ++
+        root.staticDocuments.map { doc =>
+          (doc.path, StaticResult(doc.input))
+        }.toMap
+        val roots = map.flatMap { case (path, result) =>
+          if (path.name == "index.html") Some((path.parent, result)) else None 
+        }
+        map ++ roots
       }
+  }
+
+  val transform: F[SiteResults[F]] = for { 
+    tree     <- parse
+    rendered <- transformHTML(tree)
+    ebooks   <- Async[F].fromEither(transformBinaries(tree.root).leftMap(ConfigException.apply))
+  } yield {
+    new SiteResults(rendered ++ ebooks)
   }
   
 }
 
 object SiteTransformer {
 
-  def htmlRenderer[F[_]: Async] (config: OperationConfig, theme: ThemeProvider): Resource[F, TreeRenderer[F]] = Renderer
-    .of(HTML)
-    .withConfig(config)
-    .parallel[F]
-    .withTheme(theme)
-    .build
+  def htmlRenderer[F[_]: Async] (config: OperationConfig, 
+                                 theme: ThemeProvider): Resource[F, TreeRenderer[F]] = 
+    Renderer
+      .of(HTML)
+      .withConfig(config)
+      .parallel[F]
+      .withTheme(theme)
+      .build
   
+  def binaryRenderer[F[_]: Async, FMT] (format: TwoPhaseRenderFormat[FMT, BinaryPostProcessorBuilder],
+                                        config: OperationConfig,
+                                        theme: ThemeProvider): Resource[F, BinaryTreeRenderer[F]] = 
+    Renderer
+      .of(format)
+      .withConfig(config)
+      .parallel[F]
+      .withTheme(theme)
+      .build
+
   def create[F[_]: Async](parser: Resource[F, TreeParser[F]],
                           inputs: InputTreeBuilder[F],
-                          theme: ThemeProvider): Resource[F, SiteTransformer[F]] = {
+                          theme: ThemeProvider,
+                          artifactBasename: String): Resource[F, SiteTransformer[F]] = {
     for {
-      p   <- parser
-      r   <- htmlRenderer(p.config, theme)
-    } yield new SiteTransformer[F](p,r, inputs)
+      p      <- parser
+      html   <- htmlRenderer(p.config, theme)
+      epub   <- binaryRenderer(EPUB, p.config, theme)
+      pdf    <- binaryRenderer(PDF, p.config, theme)
+    } yield new SiteTransformer[F](p, html, epub, pdf, inputs, artifactBasename)
     
   }
   
 }
+
+class SiteResults[F[_]: Async] (map: Map[Path, SiteResult[F]]) {
+  
+  def get (path: Path): Option[SiteResult[F]] = map.get(path)
+  
+}
+
+sealed abstract class SiteResult[F[_]: Async] extends Product with Serializable
+case class RenderedResult[F[_]: Async] (content: String) extends SiteResult[F]
+case class StaticResult[F[_]: Async] (content: Resource[F, InputStream]) extends SiteResult[F]
