6d71120f9bd2c0f401639e5780e9268f51f4a62c
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2021-05-01 21:27:17 +0100

    preview server: prevent browser caching of pages

--- preview/src/main/scala/laika/preview/RouteBuilder.scala
+++ preview/src/main/scala/laika/preview/RouteBuilder.scala
@@ -8,8 +8,8 @@ import fs2.io.readInputStream
 import laika.preview.ServerBuilder.Logger
 import org.http4s.EntityEncoder.entityBodyEncoder
 import org.http4s.dsl.Http4sDsl
-import org.http4s.{EntityEncoder, Headers, HttpRoutes, MediaType}
-import org.http4s.headers.`Content-Type`
+import org.http4s.{CacheDirective, EntityEncoder, Headers, HttpRoutes, MediaType}
+import org.http4s.headers.{`Cache-Control`, `Content-Type`}
 
 private [preview] class RouteBuilder[F[_]: Async](cache: Cache[F, SiteResults[F]], logger: Logger[F]) extends Http4sDsl[F] {
 
@@ -23,6 +23,8 @@ private [preview] class RouteBuilder[F[_]: Async](cache: Cache[F, SiteResults[F]
   private def mediaTypeFor (suffix: String): Option[MediaType] =
     MediaType.forExtension(suffix).orElse(MediaType.forExtension(suffix.split("\\.").last))
   
+  private val noCache = `Cache-Control`(CacheDirective.`no-store`)
+  
   def build: HttpRoutes[F] = HttpRoutes.of[F] {
 
     case GET -> path =>
@@ -30,11 +32,14 @@ private [preview] class RouteBuilder[F[_]: Async](cache: Cache[F, SiteResults[F]
       cache.get.map(_.get(laikaPath.withoutFragment)).flatMap {
         case Some(RenderedResult(content)) =>
           logger(s"serving path $laikaPath - transformed markup") *>
-          Ok(content).map(_.withContentType(`Content-Type`(MediaType.text.html)))
+          Ok(content).map(_
+            .withHeaders(noCache)
+            .withContentType(`Content-Type`(MediaType.text.html))
+          )
         case Some(StaticResult(input)) =>
           logger(s"serving path $laikaPath - static input") *> {
             val mediaType = laikaPath.suffix.flatMap(mediaTypeFor).map(`Content-Type`(_))
-            Ok(input).map(_.withHeaders(Headers(mediaType)))
+            Ok(input).map(_.withHeaders(Headers(mediaType, noCache)))
           }
         case None =>
           logger(s"serving path $laikaPath - not found") *> NotFound()
