5296277f4e072119e0ebb34bac6e900187ce6c0b
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2021-05-02 20:00:04 +0100

    add @:heliumInitPreview directive

--- io/src/main/scala/laika/helium/builder/HeliumThemeBuilder.scala
+++ io/src/main/scala/laika/helium/builder/HeliumThemeBuilder.scala
@@ -34,7 +34,7 @@ private[helium] class HeliumThemeBuilder (helium: Helium) extends ThemeProvider 
     override val description: String = "Directives for theme 'Helium'"
     val spanDirectives: Seq[Spans.Directive] = Nil
     val blockDirectives: Seq[Blocks.Directive] = Nil
-    val templateDirectives: Seq[Templates.Directive] = Seq(InitVersionsDirective.definition)
+    val templateDirectives: Seq[Templates.Directive] = HeliumDirectives.all
     val linkDirectives: Seq[Links.Directive] = Nil
   }
   
