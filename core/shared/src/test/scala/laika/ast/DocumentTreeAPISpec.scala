16b662bddcd133f126877e34264ee6b3c549a172
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2022-06-21 20:50:05 +0100

    reduce test boilerplate

--- core/shared/src/test/scala/laika/ast/DocumentTreeAPISpec.scala
+++ core/shared/src/test/scala/laika/ast/DocumentTreeAPISpec.scala
@@ -75,11 +75,16 @@ class DocumentTreeAPISpec extends FunSuite
     TreeCursor(tree)
       .map(_.children.head.asInstanceOf[TreeCursor].children.head.asInstanceOf[DocumentCursor])
 
-  def rulesFor (cursor: DocumentCursor): RewriteRules =
+  def rulesFor (cursor: DocumentCursor): ConfigResult[RewriteRules] =
     OperationConfig.default
       .rewriteRulesFor(cursor.root.target, RewritePhase.Render(OutputContext(HTML)))
       .apply(cursor)
-      .getOrElse(RewriteRules.empty)
+      
+  def rewriteWithReference (ref: String): ConfigResult[RootElement] = for {
+    cursor <- leafDocCursor(Some(ref))
+    rules  <- rulesFor(cursor)
+    result <- cursor.target.rewrite(rules)
+  } yield result.content
   
   
   test("access to the root tree when rewriting a document in the root tree") {
@@ -246,41 +251,36 @@ class DocumentTreeAPISpec extends FunSuite
   }
   
   test("resolve a substitution reference to the source path of the previous document") { 
-    val cursor = leafDocCursor(Some("cursor.previousDocument.sourcePath"))
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      rewriteWithReference("cursor.previousDocument.sourcePath"),
       Right(RootElement(p("/tree-2/doc-5")))
     )
   }
 
   test("resolve a substitution reference to the output path of the previous document") {
-    val cursor = leafDocCursor(Some("cursor.previousDocument.path"))
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      rewriteWithReference("cursor.previousDocument.path"),
       Right(RootElement(RawLink.internal(Root / "tree-2" / "doc-5")))
     )
   }
 
   test("be empty for the next document in the final leaf node of the tree") { 
-    val cursor = leafDocCursor(Some("cursor.nextDocument.path"))
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      rewriteWithReference("cursor.nextDocument.path"),
       Right(RootElement(p("")))
     )
   }
 
   test("resolve a substitution reference to the source path of the parent document") { 
-    val cursor = leafDocCursor(Some("cursor.parentDocument.sourcePath"))
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      rewriteWithReference("cursor.parentDocument.sourcePath"),
       Right(RootElement(p("/tree-2/README")))
     )
   }
 
   test("resolve a substitution reference to the output path of the parent document") {
-    val cursor = leafDocCursor(Some("cursor.parentDocument.path"))
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      rewriteWithReference("cursor.parentDocument.path"),
       Right(RootElement(RawLink.internal(Root / "tree-2" / "README")))
     )
   }
@@ -292,7 +292,7 @@ class DocumentTreeAPISpec extends FunSuite
       .get
     )
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      cursor.flatMap(c => rulesFor(c).flatMap(c.target.rewrite).map(_.content)),
       Right(RootElement(p("/tree-1/doc-4")))
     )
   }
@@ -304,7 +304,7 @@ class DocumentTreeAPISpec extends FunSuite
       .get
     )
     assertEquals(
-      cursor.flatMap(c => c.target.rewrite(rulesFor(c)).map(_.content)),
+      cursor.flatMap(c => rulesFor(c).flatMap(c.target.rewrite).map(_.content)),
       Right(RootElement(RawLink.internal(Root / "tree-1" / "doc-4")))
     )
   }
