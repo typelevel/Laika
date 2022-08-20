5b6dd9470b6260a3a6433766973b1d84968f3689
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2022-08-20 22:49:00 +0100

    update-dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ object Dependencies {
 
   object versions {
     
-    val scala2_12  = "2.12.15"
+    val scala2_12  = "2.12.16"
     val scala2_13  = "2.13.8"
-    val scala3     = "3.1.1"
+    val scala3     = "3.1.3"
 
-    val catsCore   = "2.7.0"
-    val catsEffect = "3.3.5"
-    val fs2        = "3.2.7"
-    val http4s     = "0.23.10"
+    val catsCore   = "2.8.0"
+    val catsEffect = "3.3.14"
+    val fs2        = "3.2.12"
+    val http4s     = "0.23.14"
     
     val munit      = "0.7.29"
     val munitCE3   = "1.0.7"
