0c42bacabe6637fa1e76ea2e0aaa630701c742df
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2022-09-22 00:49:29 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,8 +9,8 @@ object Dependencies {
 
     val catsCore   = "2.8.0"
     val catsEffect = "3.3.14"
-    val fs2        = "3.2.12"
-    val http4s     = "0.23.14"
+    val fs2        = "3.2.14"
+    val http4s     = "0.23.16"
     
     val munit      = "0.7.29"
     val munitCE3   = "1.0.7"
