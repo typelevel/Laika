4fddb0db70d11dba4679f055185730b9d3ac6010
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-01-25 21:23:53 +0100

    Update http4s-dsl, http4s-ember-server to 0.23.18 (#363)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val catsCore   = "2.9.0"
     val catsEffect = "3.4.5"
     val fs2        = "3.5.0"
-    val http4s     = "0.23.17"
+    val http4s     = "0.23.18"
     
     val munit      = "0.7.29"
     val munitCE3   = "1.0.7"
