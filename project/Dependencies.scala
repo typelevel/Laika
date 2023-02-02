ad26b65e0291431dc2fe61724ce15a8aa8a96e76
Scala Steward <scala_steward@virtuslab.com>
2023-02-02 19:09:12 +0000

    Update scala3-library, ... to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     
     val scala2_12  = "2.12.17"
     val scala2_13  = "2.13.10"
-    val scala3     = "3.2.1"
+    val scala3     = "3.2.2"
 
     val catsCore   = "2.9.0"
     val catsEffect = "3.4.5"
