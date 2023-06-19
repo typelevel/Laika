b2a01b06d7fb1b5fc6c732d5fbff3c4832e8b9a5
Jens Halm <3116929+jenshalm@users.noreply.github.com>
2023-06-19 21:06:59 +0100

    update http4s to 0.23.21 (#428)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsCore   = "2.9.0"
     val catsEffect = "3.5.0"
     val fs2        = "3.7.0"
-    val http4s     = "0.23.19"
+    val http4s     = "0.23.21"
 
     val munit    = "0.7.29"
     val munitCE3 = "1.0.7"
