a835ad50d9ea8fd74b28b8512af1ee8297f43fc3
Arman Bilge <armanbilge@gmail.com>
2023-09-27 05:50:46 +0000

    Bump munit

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,8 +11,8 @@ object Dependencies {
     val fs2        = "3.9.2"
     val http4s     = "0.23.23"
 
-    val munit    = "0.7.29"
-    val munitCE3 = "1.0.7"
+    val munit    = "1.0.0-M10"
+    val munitCE3 = "2.0.0-M3"
     val jTidy    = "r938"
     val fop      = "2.9"
 
